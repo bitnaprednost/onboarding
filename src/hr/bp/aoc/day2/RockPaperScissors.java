cd6a49fd36d98cda33a50dfd7b68062ead831862
Monika Domiter <monika.domiter@bitnaprednost.hr>
2023-01-02 15:53:48 +0100

    AOC - day 2 - refactoring code

--- src/hr/bp/aoc/day2/RockPaperScissors.java
+++ src/hr/bp/aoc/day2/RockPaperScissors.java
@@ -1,69 +1,63 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+package hr.bp.aoc.day2;
+
+import hr.bp.aoc.InputUtil;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class RockPaperScissors {
 
-
     public static void main(String[] args) {
         String filePath = "src/hr/bp/aoc/day2/inputday2.txt";
-        String input = readFile(filePath);
-        //System.out.println(input);
 
-        int firstScore = 0;
-        int secondScore=0;
-    
-        
-        for (String s : input.split("\n")) {
-            //System.out.println(s);
-            s.trim();
-            if(s.charAt(0)=='A' && s.charAt(2)=='X'){
-                firstScore+=1+3;
-                secondScore+=0+3;
-            }else if(s.charAt(0)=='A' && s.charAt(2)=='Y'){
-                firstScore+=2+6;
-                secondScore+=3+1;
-            }else if(s.charAt(0)=='A' && s.charAt(2)=='Z'){
-                firstScore+=3+0;
-                secondScore+=6+2;
-            }else if(s.charAt(0)=='B' && s.charAt(2)=='X'){
-                firstScore+=1+0;
-                secondScore+=0+1;
-            }else if(s.charAt(0)=='B' && s.charAt(2)=='Y'){
-                firstScore+=2+3;
-                secondScore+=3+2;
-            }else if(s.charAt(0)=='B' && s.charAt(2)=='Z'){
-                firstScore+=3+6;
-                secondScore+=6+3;
-            }else if(s.charAt(0)=='C' && s.charAt(2)=='X'){
-                firstScore+=1+6;
-                secondScore+=0+2;
-            }else if(s.charAt(0)=='C' && s.charAt(2)=='Y'){
-                firstScore+=2+0;
-                secondScore+=3+3;
-            }else if(s.charAt(0)=='C' && s.charAt(2)=='Z'){
-                firstScore+=3+3;
-                secondScore+=6+1;
-            }
+        List<String> lines = new ArrayList<>();
+        Collections.addAll(lines, InputUtil.readLines(filePath));
 
-            }
-        
-        System.out.println(firstScore);
-        System.out.println(secondScore);
-
-        
+        List<Integer> firstAndSecondScore = calculateBothScores(lines);
+        System.out.println(firstAndSecondScore);
     }
 
-    private static String readFile(String file_path) {
+    public static List<Integer> calculateBothScores(List<String> lines) {
+        int firstScore = 0;
+        int secondScore = 0;
 
-        String string = "";
-        try {
-            string = new String(Files.readAllBytes(Paths.get(file_path)));
-        } catch (IOException exception) {
-            exception.printStackTrace();
+        for (String line : lines) {
+            char opponentChoice = line.charAt(0);
+            char myChoice = line.charAt(2);
+            if (opponentChoice == 'A' && myChoice == 'X') {
+                firstScore += 1 + 3;
+                secondScore += 3;
+            } else if (opponentChoice == 'A' && myChoice == 'Y') {
+                firstScore += 2 + 6;
+                secondScore += 3 + 1;
+            } else if (opponentChoice == 'A' && myChoice == 'Z') {
+                firstScore += 3;
+                secondScore += 6 + 2;
+            } else if (opponentChoice == 'B' && myChoice == 'X') {
+                firstScore += 1;
+                secondScore += 1;
+            } else if (opponentChoice == 'B' && myChoice == 'Y') {
+                firstScore += 2 + 3;
+                secondScore += 3 + 2;
+            } else if (opponentChoice == 'B' && myChoice == 'Z') {
+                firstScore += 3 + 6;
+                secondScore += 6 + 3;
+            } else if (opponentChoice == 'C' && myChoice == 'X') {
+                firstScore += 1 + 6;
+                secondScore += 2;
+            } else if (opponentChoice == 'C' && myChoice == 'Y') {
+                firstScore += 2;
+                secondScore += 3 + 3;
+            } else if (opponentChoice == 'C' && myChoice == 'Z') {
+                firstScore += 3 + 3;
+                secondScore += 6 + 1;
+            }
         }
 
-        return string;
+        List<Integer> scores = new ArrayList<>();
+        scores.add(firstScore);
+        scores.add(secondScore);
+        return scores;
     }
-
 }
