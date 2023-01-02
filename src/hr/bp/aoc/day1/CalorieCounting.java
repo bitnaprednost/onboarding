e35dccfc811504eaf638c5795b5ba0b661035815
Monika Domiter <monika.domiter@bitnaprednost.hr>
2023-01-02 14:27:47 +0100

    AOC - day 1 - clean code

--- src/hr/bp/aoc/day1/CalorieCounting.java
+++ src/hr/bp/aoc/day1/CalorieCounting.java
@@ -1,56 +1,60 @@
 package hr.bp.aoc.day1;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import hr.bp.aoc.InputUtil;
+
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class CalorieCounting {
     public static void main(String[] args) {
+
         String filePath = "src/hr/bp/aoc/day1/inputday1.txt";
-        String input = readFile(filePath);
-        System.out.println(input);
 
+        List<String> lines = new ArrayList<>();
+
+        Collections.addAll(lines, InputUtil.readLines(filePath));
+
+        List<Integer> sumGroups = totalCaloriesCarriedByEachElf(lines);
+
+        //Part One
+        int maxCalories = maxCaloriesCarrying(sumGroups);
+        System.out.println("Elf is carrying " + maxCalories + " calories.");
+
+        //Part Two
+        int sumOfTopThreeElves = sumOfThreeElves(sumGroups);
+        System.out.println("Top three Elves are carrying " + sumOfTopThreeElves + " calories");
+
+
+    }
+
+    private static List<Integer> totalCaloriesCarriedByEachElf(List<String> lines) {
         int sumCalories = 0;
-        int maxCalories = 0;
-        ArrayList<Integer> sumGroups = new ArrayList<Integer>();
-
-        for (String s : input.split("\n")) {
-            //System.out.println(s);
-            //String strCal = s.trim();
-            if (s.isBlank()) {
-                if (maxCalories < sumCalories) {
-                    maxCalories = sumCalories;
-                }
+        List<Integer> sumGroups = new ArrayList<>();
+        for (String line : lines) {
+            if (line.isBlank()) {
                 sumGroups.add(sumCalories);
                 sumCalories = 0;
-            }else{
-                try{
-                    sumCalories += Integer.parseInt(s);
-                }
-                catch (NumberFormatException nfe){
-                    nfe.printStackTrace();
-                }
+                continue;
             }
-
-            }
-        sumGroups.add(sumCalories);
-        System.out.println("Elf is carrying "+maxCalories+" calories.");
-
-        Collections.sort(sumGroups, Collections.reverseOrder());
-        System.out.println("Top three Elves are carrying "+(sumGroups.get(0)+sumGroups.get(1)+sumGroups.get(2))+" calories");
-    }
-    private static String readFile(String file_path) {
-
-        String string = "";
-        try {
-            string = new String(Files.readAllBytes(Paths.get(file_path)));
-        } catch (IOException exception) {
-            exception.printStackTrace();
+            sumCalories += Integer.parseInt(line);
         }
-
-        return string;
+        sumGroups.add(sumCalories);
+        return sumGroups;
     }
 
+    private static int maxCaloriesCarrying(List<Integer> sumGroups) {
+        int maxCalories = 0;
+        for (Integer sumGroup : sumGroups) {
+            if (maxCalories < sumGroup) {
+                maxCalories = sumGroup;
+            }
+        }
+        return maxCalories;
+    }
+
+    private static int sumOfThreeElves(List<Integer> sumGroups) {
+        sumGroups.sort(Collections.reverseOrder());
+        return sumGroups.get(0) + sumGroups.get(1) + sumGroups.get(2);
+    }
 }
