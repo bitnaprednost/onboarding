377d9150f0339a21f530f71323fbea50c6f54f74
Monika Domiter <monika.domiter@bitnaprednost.hr>
2023-01-02 16:11:42 +0100

    AOC - day 8 - refactoring code

--- src/hr/bp/aoc/day8/TreetopTreeHouse.java
+++ src/hr/bp/aoc/day8/TreetopTreeHouse.java
@@ -1,20 +1,17 @@
 package hr.bp.aoc.day8;
 
+import hr.bp.aoc.InputUtil;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TreetopTreeHouse {
     public static void main(String[] args) {
         String filePath = "src/hr/bp/aoc/day8/inputday8.txt";
 
-        List<String> rows = new ArrayList<>();
-
-        for (String line : InputUtil.readLines(filePath)) {
-            rows.add(line);
-        }
-
+        List<String> rows = new ArrayList<>(Arrays.asList(InputUtil.readLines(filePath)));
         int[][] treeArray = asArray(rows);
-        //printMap(map,numRows,nulCol);
 
         //Part One
         int numberOfVisible = countVisible(treeArray);
@@ -40,15 +37,6 @@ public class TreetopTreeHouse {
         return map;
     }
 
-    private static void printMap(int[][] map, int numRows, int numCol) {
-        for (int i = 1; i < numRows; i++) {
-            for (int j = 1; j < numCol; j++) {
-                System.out.print(map[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
     private static int countVisible(int[][] treeArray) {
         int numRows = treeArray.length;
         int numCol = treeArray[0].length;
@@ -70,34 +58,39 @@ public class TreetopTreeHouse {
     }
 
     private static boolean isVisible(int idx1, int idx2, int[][] map, int numRows, int numCol, String direction) {
-        if (direction.equals("right")) {
-            for (int j = numCol - 1; j > idx2; j--) {
-                if (map[idx1][j] >= map[idx1][idx2]) {
-                    return false;
+        switch (direction) {
+            case "right" -> {
+                for (int j = numCol - 1; j > idx2; j--) {
+                    if (map[idx1][j] >= map[idx1][idx2]) {
+                        return false;
+                    }
                 }
+                return true;
             }
-            return true;
-        } else if (direction.equals("left")) {
-            for (int j = 0; j < idx2; j++) {
-                if (map[idx1][j] >= map[idx1][idx2]) {
-                    return false;
+            case "left" -> {
+                for (int j = 0; j < idx2; j++) {
+                    if (map[idx1][j] >= map[idx1][idx2]) {
+                        return false;
+                    }
                 }
+                return true;
             }
-            return true;
-        } else if (direction.equals("up")) {
-            for (int i = 0; i < idx1; i++) {
-                if (map[i][idx2] >= map[idx1][idx2]) {
-                    return false;
+            case "up" -> {
+                for (int i = 0; i < idx1; i++) {
+                    if (map[i][idx2] >= map[idx1][idx2]) {
+                        return false;
+                    }
                 }
+                return true;
             }
-            return true;
-        } else if (direction.equals("down")) {
-            for (int i = numRows - 1; i > idx1; i--) {
-                if (map[i][idx2] >= map[idx1][idx2]) {
-                    return false;
+            case "down" -> {
+                for (int i = numRows - 1; i > idx1; i--) {
+                    if (map[i][idx2] >= map[idx1][idx2]) {
+                        return false;
+                    }
                 }
+                return true;
             }
-            return true;
         }
         return false;
     }
