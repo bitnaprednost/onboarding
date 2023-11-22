4c3f164b8e601364954607c4dec6ab149e4299e2
LukaLjubic <luka@ljubic.me>
2023-11-22 12:36:03 +0100

    Created and completed day four

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.days.day03.DayThree;
+import org.example.days.day04.DayFour;
 
 import java.io.IOException;
 import java.util.List;
@@ -16,28 +17,12 @@ public class Main {
         //DayTwo dayTwo = new DayTwo();
         //dayTwo.executeDay();
 
-        DayThree dayThree = new DayThree();
-        dayThree.executePartTwo();
-       /* String input = Files.readString(Path.of("src/main/resources/backpackData.txt"));
+        /*DayThree dayThree = new DayThree();
+        dayThree.executePartTwo();*/
 
-
-
-        var counter = IntStream.range(0, input.length()).iterator();
-        var sum2 = input.lines()
-                .collect(Collectors.groupingBy(c -> counter.nextInt()/3)).values().stream()
-                .mapToInt(l -> commonPriority(l)).sum();
-        System.out.printf("part 2: %d\n", sum2);*/
+        DayFour dayFour = new DayFour();
+        dayFour.executePartTwo();
     }
 
-    static int commonPriority(List<String> strings) {
-        // common character in list of strings
-        var c = strings.stream().reduce((s1, s2) -> commonChars(s1, s2)).get().charAt(0);
-        return c < 'a' ? c - 'A' + 27 : c - 'a' + 1; // convert to priority
-    }
-
-    static String commonChars(String a, String b) {
-        return a.chars().filter(i1 -> b.chars().anyMatch(i2 -> i1 == i2)).distinct()
-                .mapToObj(c -> Character.toString(c)).collect(Collectors.joining());
-    }
 
 }
