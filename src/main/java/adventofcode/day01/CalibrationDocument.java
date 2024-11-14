51d76e49588eda0502a9725b5c2a30fda4c086d9
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-11-14 12:41:25 +0100

    refactored implementation for calculating the sum

--- src/main/java/adventofcode/day01/CalibrationDocument.java
+++ src/main/java/adventofcode/day01/CalibrationDocument.java
@@ -1,6 +1,5 @@
 package adventofcode.day01;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,6 +16,38 @@ public class CalibrationDocument {
     }
 
     public int calculateSumOfCalibrationValues() {
-        return 197;
+        int sum = 0;
+        for (String line : lines) {
+            sum += extractCalibrationValueFromLine(line);
+        }
+        return sum;
+    }
+
+    private int extractCalibrationValueFromLine(String line) {
+        int leftNumber = getLeftNumber(line);
+        int rightNumber = getRightNumber(line);
+        return leftNumber*10 + rightNumber;
+    }
+
+    private int getLeftNumber(String line) {
+        char[] lineAsCharArray = line.toCharArray();
+        for (int i = 0; i < lineAsCharArray.length ; i++) {
+            char character = lineAsCharArray[i];
+            if (Character.isDigit(character)) {
+                return Integer.parseInt(Character.toString(character));
+            }
+        }
+        return Integer.MIN_VALUE;
+    }
+
+    private int getRightNumber(String line) {
+        char[] lineAsCharArray = line.toCharArray();
+        for (int i = lineAsCharArray.length - 1; i >=0 ; i--) {
+            char character = lineAsCharArray[i];
+            if (Character.isDigit(character)) {
+                return Integer.parseInt(Character.toString(character));
+            }
+        }
+        return Integer.MIN_VALUE;
     }
 }
