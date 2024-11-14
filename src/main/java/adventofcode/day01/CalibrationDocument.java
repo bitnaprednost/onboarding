8adc4d1fbaf33f6693dbdb05a0553c70387d09e9
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-11-14 12:31:23 +0100

    added member varaible lines and setter for it

--- src/main/java/adventofcode/day01/CalibrationDocument.java
+++ src/main/java/adventofcode/day01/CalibrationDocument.java
@@ -1,5 +1,12 @@
 package adventofcode.day01;
 
+import java.util.List;
+
 public class CalibrationDocument {
 
+    private List<String> lines;
+
+    public void setLines(List<String> lines) {
+        this.lines = lines;
+    }
 }
