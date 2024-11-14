2da1adf7e21b6f29138e44189c46460cc59033fe
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-11-14 12:27:31 +0100

    added first test for setting lines for CalibrationDocument

--- src/test/java/adventofcode/day01/CalibrationDocumentTest.java
+++ src/test/java/adventofcode/day01/CalibrationDocumentTest.java
@@ -1,11 +1,24 @@
 package adventofcode.day01;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class CalibrationDocumentTest {
 
     private CalibrationDocument calibrationDocument;
 
+    @BeforeEach
+    public void setUp() {
+        this.calibrationDocument = new CalibrationDocument();
+    }
 
+    @Test
+    public void testSettingLines() {
+        List<String> inputLines = Arrays.asList("123213", "adsad11", "sdsdsd");
+        calibrationDocument.setLines(inputLines);
+    }
 }
