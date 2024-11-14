91a8614b4a8b4f46f17517296bb5caadcf6b8442
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-11-14 13:40:24 +0100

    fixed main function

--- src/main/java/adventofcode/day01/TaskOne.java
+++ src/main/java/adventofcode/day01/TaskOne.java
@@ -1,22 +1,25 @@
 package adventofcode.day01;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TaskOne {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        StringBuilder stringBuilder = new StringBuilder();
+        List<String> lines = new ArrayList<>();
         while (true) {
             String inputLine = scanner.nextLine();
             if (inputLine.isBlank()) {
                 break;
             }
-            stringBuilder.append(inputLine);
+            lines.add(inputLine);
         }
+
         CalibrationDocument calibrationDocument = new CalibrationDocument();
-        calibrationDocument.setLinesFromInputString(stringBuilder.toString());
+        calibrationDocument.setLines(lines);
         int sum = calibrationDocument.calculateCalibrationValuesSum();
         calibrationDocument.displayCalibrationValuesSum(sum);
     }
