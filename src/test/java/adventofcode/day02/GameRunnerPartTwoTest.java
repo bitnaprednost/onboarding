12f201c3a11df2c63a28efbc87f96c9250498599
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-11-15 15:21:47 +0100

    add a failing test for the class for the 2nd part of the task

---
+++ src/test/java/adventofcode/day02/GameRunnerPartTwoTest.java
@@ -0,0 +1,30 @@
+package adventofcode.day02;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static adventofcode.day02.TestUtils.getShortInput;
+
+public class GameRunnerPartTwoTest {
+
+    private GameRunnerPartTwo gameRunnerPartTwo;
+    private InputParser inputParser;
+
+    @BeforeEach
+    public void setUp() {
+        inputParser = new InputParser();
+        gameRunnerPartTwo = new GameRunnerPartTwo(inputParser);
+    }
+
+    @Test
+    public void testSmallMap() {
+        String input = getShortInput();
+        Integer expectedIdSum = 2286;
+
+        Integer actualIdSum = gameRunnerPartTwo.calculateSumOfPowerOfSets(input);
+
+        Assertions.assertEquals(expectedIdSum, actualIdSum);
+    }
+
+}
