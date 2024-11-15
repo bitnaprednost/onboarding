a0c5e71193dd62a0a57333fc55a6833b95e83130
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-11-15 15:01:29 +0100

    add class representing one game (one line in task input)

---
+++ src/main/java/adventofcode/day02/Game.java
@@ -0,0 +1,27 @@
+package adventofcode.day02;
+
+import java.util.List;
+import java.util.Map;
+
+public class Game {
+    private final int gameId;
+    private final List<Map<ColorEnum, Integer>> subsets;
+
+    public Game(int gameId, List<Map<ColorEnum, Integer>> subsets) {
+        this.gameId = gameId;
+        this.subsets = subsets;
+    }
+
+    public int getGameId() {
+        return gameId;
+    }
+
+    public boolean isPossibleWithBag(Bag bag) {
+        for (Map<ColorEnum, Integer> subset : subsets) {
+            if (!bag.canHoldSubset(subset)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
