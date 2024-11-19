cc40287dc49d2dd5e2866f42bbcd7df172b717ad
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-11-19 12:17:08 +0100

    added class for running task 2 day 03

--- src/main/java/adventofcode/day03/TaskTwo.java
+++ src/main/java/adventofcode/day03/TaskTwo.java
@@ -1,4 +1,12 @@
 package adventofcode.day03;
 
+import adventofcode.Utils;
+
 public class TaskTwo {
+    public static void main(String[] args) {
+        String input = Utils.readUserInputAsString();
+        EngineSchematic engineSchematic = new EngineSchematic(input);
+        int sum = engineSchematic.calculateSumOfGearRatios();
+        Utils.displayResult(sum);
+    }
 }
