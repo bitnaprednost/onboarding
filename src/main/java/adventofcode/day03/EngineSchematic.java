265022e7ea0df870ba9d2e5bb47eb4f7a64c6927
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-11-19 10:43:40 +0100

    implemented method that checks whether a piece of grid is adjacent to a symbol or not

--- src/main/java/adventofcode/day03/EngineSchematic.java
+++ src/main/java/adventofcode/day03/EngineSchematic.java
@@ -9,7 +9,6 @@ public class EngineSchematic {
             throw new IllegalArgumentException("Input cannot be null or blank.");
         }
         this.parseInput(input);
-
     }
 
     public int calculateSum() {
@@ -17,9 +16,31 @@ public class EngineSchematic {
     }
 
     public boolean isAdjacentToSymbol(int rowIndex, int startColumnIndex, int endColumnIndex) {
-        return true;
+        boolean isAdjacent = checkRowForSymbols(rowIndex, startColumnIndex, endColumnIndex);
+        if (rowIndex > 0) {
+            isAdjacent |= checkRowForSymbols(rowIndex - 1, startColumnIndex, endColumnIndex);
+        }
+        if (rowIndex < grid.length - 1) {
+            isAdjacent |= checkRowForSymbols(rowIndex + 1, startColumnIndex, endColumnIndex);
+        }
+        return isAdjacent;
     }
 
+    private boolean checkRowForSymbols(int rowIndex, int startColumnIndex, int endColumnIndex) {
+        char[] row = grid[rowIndex];
+        startColumnIndex = Math.max(0, startColumnIndex - 1);
+        endColumnIndex = Math.min(row.length - 1, endColumnIndex + 1);
+        for (int i = startColumnIndex; i <= endColumnIndex; i++) {
+            if (isSymbol(row[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isSymbol(char c) {
+        return !Character.isDigit(c) && !String.valueOf(c).equals(".");
+    }
 
     private void parseInput(String input) {
         String[] rows = input.split("\n");
