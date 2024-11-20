532afa5acef57b040369b191a3dd56a18abe7bb0
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-11-20 10:48:25 +0100

    added parsing of input into scratchcards list (task 1 day 04)

--- src/main/java/adventofcode/day04/Game.java
+++ src/main/java/adventofcode/day04/Game.java
@@ -1,12 +1,16 @@
 package adventofcode.day04;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author Ivan Tomičić
  */
 public class Game {
-    private List<Scratchcard> scratchcards;
+    private final List<Scratchcard> scratchcards = new ArrayList<>();
 
     public Game(String input) {
         if (input == null || input.isBlank()) {
@@ -16,7 +20,27 @@ public class Game {
     }
 
     private void setScratchcardsFromInput(String input) {
+        String[] lines = input.split("\n");
+        for (String line : lines) {
+            addScratchcardFromLine(line);
+        }
 
+    }
+
+    private void addScratchcardFromLine(String line) {
+        String leftSide = line.split("\\|")[0].split(":")[1].strip();
+        String rightSide = line.split("\\|")[1].strip();
+
+        Set<Integer> winningNumbers = splitStringBySpace(leftSide);
+        Set<Integer> playersNumbers = splitStringBySpace(rightSide);
+
+        scratchcards.add(new Scratchcard(winningNumbers, playersNumbers));
+    }
+
+    private Set<Integer> splitStringBySpace(String numbersString) {
+        return Arrays.stream(numbersString.split("\\s+"))
+                .map(value -> Integer.parseInt(value.strip()))
+                .collect(Collectors.toSet());
     }
 
     public List<Scratchcard> getScratchcards() {
