41d34732e6559a3d02ff3a203b3c45a6cadb6505
marijahorvat171 <marijahorvat171@gmail.com>
2024-11-20 10:11:13 +0100

    add aoc day2 solution

---
+++ aoc/day2/Main.java
@@ -0,0 +1,28 @@
+package day2;
+
+import common.ReadFile;
+import day1.CalibrationSum;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Specify the file name
+        String fileName = "day2/games.txt";
+
+        // Read the file
+        ReadFile readFile = new ReadFile();
+        String fileContent = readFile.readFile(fileName);
+
+        // Calculate the sum of games Ids
+        GameIDSum gameID = new GameIDSum();
+        int totalSum = gameID.gameIDSum(fileContent);
+
+        System.out.println("Total sum of game IDs is: " + totalSum);
+
+        // Calculate the power sum
+        PowerSum power = new PowerSum();
+        int totalPowerSum = power.powerSum(fileContent);
+
+        System.out.println("Total sum of the power of sets: " + totalPowerSum);
+    }
+}
