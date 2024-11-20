32009297ef65bc5304e74fc11fea0e37b31f2b03
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-11-20 11:02:09 +0100

    added code for running the first task (day 04)

---
+++ src/main/java/adventofcode/day04/TaskOne.java
@@ -0,0 +1,15 @@
+package adventofcode.day04;
+
+import adventofcode.Utils;
+
+/**
+ * @author Ivan Tomičić
+ */
+public class TaskOne {
+    public static void main(String[] args) {
+        String input = Utils.readUserInputAsString();
+        Game game = new Game(input);
+        int sum = game.calculateTotalPoints();
+        Utils.displayResult(sum);
+    }
+}
