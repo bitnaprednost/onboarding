95722c345769a686fa2b96e3efff143a16f358b9
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-11-20 18:26:38 +0100

    added implementation of parsing input into a list of maps

--- src/main/java/adventofcode/day05/Almanac.java
+++ src/main/java/adventofcode/day05/Almanac.java
@@ -1,5 +1,8 @@
 package adventofcode.day05;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -7,50 +10,55 @@ import java.util.List;
  */
 public class Almanac {
 
-    private List<Integer> seeds;
+    private List<Integer> seeds = new ArrayList<>();
 
-    private List<SourceDestinationMap> seedToSoilMap;
-    private List<SourceDestinationMap> soilToFertilizerMap;
-    private List<SourceDestinationMap> fertilizerToWaterMap;
-    private List<SourceDestinationMap> waterToLightMap;
-    private List<SourceDestinationMap> lightToTemperatureMap;
-    private List<SourceDestinationMap> temperatureToHumidityMap;
-    private List<SourceDestinationMap> humidityToLocationMap;
+    private List<List<SourceDestinationMap>> listOfMaps = new LinkedList<>();
 
     public Almanac(String input) {
-
-
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException("Input cannot be null or blank");
+        }
+        setSeedsAndMapsFromInput(input);
     }
 
-    public List<SourceDestinationMap> getTemperatureToHumidityMap() {
-        return temperatureToHumidityMap;
+    private void setSeedsAndMapsFromInput(String input) {
+        String[] inputSections = input.split("\n\n");
+
+        parseSeedsInput(inputSections[0]);
+
+        for (int i = 1; i < inputSections.length; i++) {
+            parseMap(inputSections[i]);
+        }
+        System.out.println(listOfMaps);
+    }
+
+    private void parseMap(String mapSection) {
+        List<SourceDestinationMap> map = new ArrayList<>();
+
+        String[] sourceDestinationLists = mapSection
+                .split(":\n")[1].strip()
+                .split("\n");
+
+        for (String sourceDestinationLine : sourceDestinationLists) {
+            map.add(SourceDestinationMap.createFromInputLine(sourceDestinationLine));
+        }
+
+        listOfMaps.add(map);
+    }
+
+    private void parseSeedsInput(String seedsSection) {
+        String[] seedNumbers = seedsSection.split(":")[1].strip().split("\\s+");
+        Arrays.stream(seedNumbers)
+                .forEach(
+                        e -> seeds.add(Integer.parseInt(e))
+                );
+    }
+
+    public List<List<SourceDestinationMap>> getListOfMaps() {
+        return listOfMaps;
     }
 
     public List<Integer> getSeeds() {
         return seeds;
-    }
-
-    public List<SourceDestinationMap> getSeedToSoilMap() {
-        return seedToSoilMap;
-    }
-
-    public List<SourceDestinationMap> getSoilToFertilizerMap() {
-        return soilToFertilizerMap;
-    }
-
-    public List<SourceDestinationMap> getFertilizerToWaterMap() {
-        return fertilizerToWaterMap;
-    }
-
-    public List<SourceDestinationMap> getWaterToLightMap() {
-        return waterToLightMap;
-    }
-
-    public List<SourceDestinationMap> getLightToTemperatureMap() {
-        return lightToTemperatureMap;
-    }
-
-    public List<SourceDestinationMap> getHumidityToLocationMap() {
-        return humidityToLocationMap;
     }
 }
--- src/main/java/adventofcode/day05/SourceDestinationMap.java
+++ src/main/java/adventofcode/day05/SourceDestinationMap.java
@@ -1,5 +1,7 @@
 package adventofcode.day05;
 
+import java.util.Objects;
+
 /**
  * @author Ivan Tomičić
  */
@@ -13,5 +15,34 @@ public class SourceDestinationMap {
         this.sourceStart = sourceStart;
         this.destinationStart = destinationStart;
         this.rangeLength = rangeLength;
+    }
+
+    public static SourceDestinationMap createFromInputLine(String inputLine) {
+        String[] parameters = inputLine.split(" ");
+        return new SourceDestinationMap(
+                Integer.parseInt(parameters[0]),
+                Integer.parseInt(parameters[1]),
+                Integer.parseInt(parameters[2])
+        );
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof SourceDestinationMap that)) return false;
+        return sourceStart == that.sourceStart && destinationStart == that.destinationStart && rangeLength == that.rangeLength;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sourceStart, destinationStart, rangeLength);
+    }
+
+    @Override
+    public String toString() {
+        return "SourceDestinationMap{" +
+                "sourceStart=" + sourceStart +
+                ", destinationStart=" + destinationStart +
+                ", rangeLength=" + rangeLength +
+                '}';
     }
 }
