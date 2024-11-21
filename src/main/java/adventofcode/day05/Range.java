2699758235ca2f9a86ba0220b550c4af9aba1ee7
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-11-21 16:45:31 +0100

    removed unnecessary methods from Range, added mapToNextLevel method

--- src/main/java/adventofcode/day05/Range.java
+++ src/main/java/adventofcode/day05/Range.java
@@ -14,14 +14,6 @@ public record Range (long start, long end, Function<Long, Long> mappingFunction)
     public static final Function<Long,Long> DEFAULT_MAPPER = a -> a;
     public static final Comparator<Range> RANGE_COMPARATOR = Comparator.comparingLong(o -> o.start);
 
-    public long rangeSize() {
-        return end + start + 1;
-    }
-
-    public static boolean numberIsInRange(long number, Range range) {
-        return number >= range.start && number <= range.end;
-    }
-
     public static List<Range> breakRangeIntoRanges(Range range, List<Range> mapperRanges) {
         List<Range> newRanges = new ArrayList<>();
         List<Range> sortedMapperRanges = new ArrayList<>(List.copyOf(mapperRanges));
@@ -68,8 +60,17 @@ public record Range (long start, long end, Function<Long, Long> mappingFunction)
         newRanges.sort(RANGE_COMPARATOR);
     }
 
-    public static Range mapToNextLevel(Range range, Function<Long, Long> mapper) {
-        return new Range(mapper.apply(range.start), mapper.apply(range.end), DEFAULT_MAPPER);
+    public static List<Range> mapToNextLevel(List<Range> currentRanges) {
+        List<Range> nextLevelRanges = new ArrayList<>();
+        for (Range currentRange : currentRanges) {
+            Range nextLevelRange = new Range(
+                    currentRange.mappingFunction.apply(currentRange.start),
+                    currentRange.mappingFunction.apply(currentRange.end),
+                    DEFAULT_MAPPER
+            );
+            nextLevelRanges.add(nextLevelRange);
+        }
+        return nextLevelRanges;
     }
 
     public static Optional<Range> getOverlappedRange(Range seedRange, Range mapperRange) {
