2f3c75453c02f679494cf6df80d8ad05ea86db59
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-11-21 16:46:58 +0100

    added implementation for the 2nd task (it works)

--- src/main/java/adventofcode/day05/AlmanacWithRanges.java
+++ src/main/java/adventofcode/day05/AlmanacWithRanges.java
@@ -1,5 +1,8 @@
 package adventofcode.day05;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Ivan Tomičić
  */
@@ -11,7 +14,39 @@ public class AlmanacWithRanges extends Almanac {
 
     @Override
     public long findLowestLocationNumber() {
-        long lowestLocationNumber = 46;
-        return lowestLocationNumber;
+        List<Range> seedsRanges = new ArrayList<>();
+        for (int i = 0; i < seeds.size(); i += 2) {
+            seedsRanges.add(
+                    new Range(seeds.get(i), seeds.get(i) + seeds.get(i + 1) - 1, Range.DEFAULT_MAPPER)
+            );
+        }
+        seedsRanges.sort(Range.RANGE_COMPARATOR);
+
+        for (List<SourceDestinationMap> listOfMap : listOfMaps) {
+            List<Range> currentLevelRanges = new ArrayList<>();
+            for (Range seedRange : seedsRanges) {
+                List<Range> mapperRanges = extractMapperRanges(listOfMap);
+                List<Range> overlappedRanges = Range.breakRangeIntoRanges(seedRange, mapperRanges);
+                currentLevelRanges.addAll(overlappedRanges);
+            }
+            seedsRanges = Range.mapToNextLevel(currentLevelRanges);
+        }
+
+        return seedsRanges
+                .stream()
+                .mapToLong(Range::start)
+                .min().getAsLong();
+    }
+
+    private List<Range> extractMapperRanges(List<SourceDestinationMap> sourceDestinationMaps) {
+        List<Range> mapperRanges = new ArrayList<>();
+        for (SourceDestinationMap map : sourceDestinationMaps) {
+            Range mapperRange = new Range(
+                    map.sourceStart(),
+                    map.sourceStart() + map.rangeLength(),
+                    a -> a + map.destinationStart() - map.sourceStart());
+            mapperRanges.add(mapperRange);
+        }
+        return mapperRanges;
     }
 }
