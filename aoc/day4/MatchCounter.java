7e74998c2707eddf321dc819fd00940508acce28
marijahorvat171 <marijahorvat171@gmail.com>
2024-11-22 15:19:39 +0100

    add solution for day4

---
+++ aoc/day4/MatchCounter.java
@@ -0,0 +1,37 @@
+package day4;
+
+public class MatchCounter {
+    private int matchCount;
+
+    public MatchCounter(int[] winningNumbers, int[] playerNumbers) {
+        this.matchCount = calculateMatchCount(winningNumbers, playerNumbers);
+    }
+
+    private int calculateMatchCount(int[] winningNumbers, int[] playerNumbers) {
+        int i = 0, j = 0, count = 0;
+
+        while (i < winningNumbers.length && j < playerNumbers.length)
+        {
+            if (winningNumbers[i] == playerNumbers[j])
+            {
+                count++;
+                i++;
+                j++;
+            }
+            else if (winningNumbers[i] < playerNumbers[j])
+            {
+                i++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+
+        return count;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+}
---
+++ aoc/day4/TotalPoint.java
@@ -0,0 +1,36 @@
+package day4;
+
+public class TotalPoint {
+    public int totalPoint (String filename){
+        int  totalSum = 0;
+
+        // Split the file content into lines
+        String[] lines = filename.split(System.lineSeparator());
+
+        // Process each line
+        for (String line : lines)
+        {
+            // Use CardParser to parse the line and get arrays
+            CardParser parser = new CardParser(line);
+
+            // Use MatchCounter to calculate the number of matches
+            MatchCounter counter = new MatchCounter(parser.getWinningNumbers(), parser.getPlayerNumbers());
+            int matchCount = counter.getMatchCount();
+
+            int cardValue = 0;
+
+            // Calculate the card's value based on the number of matches
+            if (matchCount > 0)
+            {
+                cardValue = 1; // First match
+                for (int k = 1; k < matchCount; k++)
+                {
+                    cardValue *= 2; // Double for each subsequent match
+                }
+            }
+
+            totalSum += cardValue;
+        }
+        return totalSum;
+    }
+}
