6ecfb318d9341f67ddd9245f1896bbb6113e3944
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-11-22 15:04:39 +0100

    wrote a failing test for a method which should calculate the final result for the task 1, renamed variables in the test class and the test class

---
+++ src/test/java/hr/bp/adventofcode/day06/RacesRunnerTest.java
@@ -0,0 +1,53 @@
+package hr.bp.adventofcode.day06;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static hr.bp.adventofcode.day06.TestUtils.getFirstInput;
+import static hr.bp.adventofcode.day06.TestUtils.getFirstInputParsed;
+import static hr.bp.adventofcode.day06.TestUtils.getSecondInput;
+import static hr.bp.adventofcode.day06.TestUtils.getSecondInputParsed;
+
+/**
+ * @author Ivan Tomičić
+ */
+public class RacesRunnerTest {
+
+    @Test
+    public void parseRaces_givenTheFirstRaceInput_returnsCorrectParsedRaces() {
+        String input = getFirstInput();
+        RacesRunner racesRunner = new RacesRunner(input);
+
+        List<RaceRecord> expectedRaceRecords = getFirstInputParsed();
+
+        List<RaceRecord> actualRaceRecords = racesRunner.getRaceRecords();
+
+        Assertions.assertEquals(expectedRaceRecords, actualRaceRecords);
+    }
+
+    @Test
+    public void parseRaces_givenTheSecondRaceInput_returnsCorrectParsedRaces() {
+        String input = getSecondInput();
+        RacesRunner racesRunner = new RacesRunner(input);
+
+        List<RaceRecord> expectedRaceRecords = getSecondInputParsed();
+
+        List<RaceRecord> actualRaceRecords = racesRunner.getRaceRecords();
+
+        Assertions.assertEquals(expectedRaceRecords, actualRaceRecords);
+    }
+
+    @Test
+    public void multiplyResultsOfAllRaces_givenTheFirstRaceInput_returnsCorrectResult() {
+        String input = getFirstInput();
+        RacesRunner racesRunner = new RacesRunner(input);
+
+        int expectedResult = 288;
+
+        int actualResult = racesRunner.multiplyResultsOfAllRaces();
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+}
