201d11f0ab079c449bdc93362e4ee7ec35413258
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-11-22 15:11:55 +0100

    wrote a class for running the 1st task

---
+++ src/main/java/hr/bp/adventofcode/day06/TaskOne.java
@@ -0,0 +1,19 @@
+package hr.bp.adventofcode.day06;
+
+import static adventofcode.Utils.displayResult;
+import static adventofcode.Utils.readUserInputAsString;
+
+/**
+ * @author Ivan Tomičić
+ */
+public class TaskOne {
+
+    public static void main(String[] args) {
+        String input = readUserInputAsString();
+        RacesRunner racesRunner = new RacesRunner(input);
+
+        int sum = racesRunner.multiplyResultsOfAllRaces();
+
+        displayResult(sum);
+    }
+}
