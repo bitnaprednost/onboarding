116ff2303c9e1f518c15b7c2db017276f8699bd8
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-11-22 14:21:51 +0100

    added a class that represents a race

---
+++ src/main/java/hr/bp/adventofcode/day06/Race.java
@@ -0,0 +1,18 @@
+package hr.bp.adventofcode.day06;
+
+/**
+ * @author Ivan Tomičić
+ */
+public class Race {
+
+    RaceRecord raceRecord;
+
+    public Race(RaceRecord raceRecord) {
+        this.raceRecord = raceRecord;
+    }
+
+    public int calculateWaysToWinARace() {
+        return 4;
+    }
+
+}
