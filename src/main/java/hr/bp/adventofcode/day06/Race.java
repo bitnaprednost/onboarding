82ba4a861de9416a8426600bf8181cd1bc704fff
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-11-22 14:46:10 +0100

    wrote a Race class

--- src/main/java/hr/bp/adventofcode/day06/Race.java
+++ src/main/java/hr/bp/adventofcode/day06/Race.java
@@ -1,5 +1,7 @@
 package hr.bp.adventofcode.day06;
 
+import org.graalvm.collections.Pair;
+
 /**
  * @author Ivan Tomičić
  */
@@ -12,7 +14,37 @@ public class Race {
     }
 
     public int calculateWaysToWinARace() {
-        return 4;
+        int maximumTime = raceRecord.maximumTime();
+        int recordDistance = raceRecord.recordDistance();
+
+        Pair<Double, Double> quadraticEquationResult = solveQuadraticEquation(maximumTime, recordDistance);
+
+        Pair<Integer, Integer> extractedWholeNumbers = extractWholeNumbers(quadraticEquationResult);
+
+        return extractedWholeNumbers.getRight() - extractedWholeNumbers.getLeft() + 1;
+    }
+
+
+    private Pair<Double, Double> solveQuadraticEquation(int maximumTime, int recordDistance) {
+        Double rightValue = (((maximumTime*maximumTime) + Math.sqrt(maximumTime * maximumTime - 4 * recordDistance)) / 2);
+        Double leftValue = (((maximumTime*maximumTime) - Math.sqrt(maximumTime*maximumTime - 4*recordDistance)) / 2);
+        
+        return Pair.create(leftValue, rightValue);
+    }
+
+    private Pair<Integer, Integer> extractWholeNumbers(Pair<Double, Double> quadraticEquationResult) {
+        Double leftValue = quadraticEquationResult.getLeft();
+        Double rightValue = quadraticEquationResult.getRight();
+
+        Integer leftValueCeiled = (leftValue % 1 == 0)
+                ? (int) Math.ceil(leftValue) + 1
+                : (int) Math.ceil(leftValue);
+
+        Integer rightValueFloored = (rightValue % 1 == 0)
+                ? (int) Math.floor(rightValue) - 1
+                : (int) Math.floor(rightValue);
+
+        return Pair.create(leftValueCeiled, rightValueFloored);
     }
 
 }
