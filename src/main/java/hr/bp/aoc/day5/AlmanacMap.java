18a07a7cd450890814d004d1e44f6579d9f9c6b1
Nikolina Spehar <speharnikolina87@gmail.com>
2024-11-27 12:35:34 +0100

    refactoring - adding range end to AlmanacMap

--- src/main/java/hr/bp/aoc/day5/AlmanacMap.java
+++ src/main/java/hr/bp/aoc/day5/AlmanacMap.java
@@ -2,6 +2,8 @@ package hr.bp.aoc.day5;
 
 public class AlmanacMap {
     private long sourceStart;
+    private long sourceEnd;
+    private long mapping;
     private long destinationStart;
     private long range;
 
@@ -9,6 +11,8 @@ public class AlmanacMap {
         this.sourceStart = sourceStart;
         this.destinationStart = destinationStart;
         this.range = range;
+        sourceEnd = sourceStart + range - 1;
+        mapping = sourceStart - destinationStart;
     }
 
     public long getSourceStart() {
