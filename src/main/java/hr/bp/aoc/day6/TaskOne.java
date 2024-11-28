cec0d6e337938321f6bb74603afbe718fb005cac
Nikolina Spehar <speharnikolina87@gmail.com>
2024-11-28 10:54:28 +0100

    new - adding main method for testing task 1

---
+++ src/main/java/hr/bp/aoc/day6/TaskOne.java
@@ -0,0 +1,13 @@
+package hr.bp.aoc.day6;
+
+import hr.bp.aoc.day1.Utils;
+
+import java.util.List;
+
+
+public class TaskOne {
+    public static void main(String[] args) {
+        List<String> document = Utils.readFromFile("inputDay6.txt");
+
+    }
+}
