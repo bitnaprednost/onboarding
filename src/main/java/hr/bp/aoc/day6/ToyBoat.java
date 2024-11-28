00e58fbde87e91a0a8014d0f6d098ea994fe3e15
Nikolina Spehar <speharnikolina87@gmail.com>
2024-11-28 12:10:18 +0100

    new - added ToyBoat class

---
+++ src/main/java/hr/bp/aoc/day6/ToyBoat.java
@@ -0,0 +1,27 @@
+package hr.bp.aoc.day6;
+
+public class ToyBoat {
+    private int speed;
+    private int raceTime;
+
+    public ToyBoat(int timeSpentHoldingButton, int raceTime) {
+        this.speed = timeSpentHoldingButton;
+        this.raceTime = raceTime;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    private int getRemainingRaceTime() {
+        return raceTime - speed;
+    }
+
+    public int getTraveledDistance() {
+        return speed * getRemainingRaceTime();
+    }
+
+    public boolean raceWon(Race race) {
+        return getTraveledDistance() > race.getRecordDistance();
+    }
+}
