2ac37ccc8e0f013e7f18e9fd8af9621f6bd1928a
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-11-28 09:26:25 +0100

    added a test for expected parsing of input

--- src/test/java/hr/bp/adventofcode/day10/PipeMazeTest.java
+++ src/test/java/hr/bp/adventofcode/day10/PipeMazeTest.java
@@ -1,6 +1,10 @@
 package hr.bp.adventofcode.day10;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static hr.bp.adventofcode.day10.TestUtils.getFirstInput;
+import static hr.bp.adventofcode.day10.TestUtils.getFirstInputGrid;
 
 /**
  * @author Ivan Tomičić
@@ -9,6 +13,13 @@ public class PipeMazeTest {
 
     @Test
     public void parseInput_givenValidInputOne_parsesCorrectly() {
+        String input = getFirstInput();
+        PipeMaze pipeMaze = new PipeMaze(input);
 
+        GridElement[][] expectedGrid = getFirstInputGrid();
+
+        GridElement[][] actualGrid = pipeMaze.getGrid();
+
+        Assertions.assertArrayEquals(expectedGrid, actualGrid);
     }
 }
--- src/test/java/hr/bp/adventofcode/day10/TestUtils.java
+++ src/test/java/hr/bp/adventofcode/day10/TestUtils.java
@@ -14,6 +14,16 @@ public class TestUtils {
             LJ...""";
     }
 
+    public static GridElement[][] getFirstInputGrid() {
+        return new GridElement[][] {
+                new GridElement[] {GridElement.GROUND, GridElement.GROUND, GridElement.SOUTH_EAST, GridElement.SOUTH_WEST, GridElement.GROUND},
+                new GridElement[] {GridElement.GROUND, GridElement.SOUTH_EAST, GridElement.NORTH_WEST, GridElement.NORTH_SOUTH, GridElement.GROUND},
+                new GridElement[] {GridElement.STARTING_POSITION, GridElement.NORTH_WEST, GridElement.GROUND, GridElement.NORTH_EAST, GridElement.SOUTH_WEST},
+                new GridElement[] {GridElement.NORTH_SOUTH, GridElement.SOUTH_EAST, GridElement.EAST_WEST, GridElement.EAST_WEST, GridElement.NORTH_WEST},
+                new GridElement[] {GridElement.NORTH_EAST, GridElement.NORTH_WEST, GridElement.GROUND, GridElement.GROUND, GridElement.GROUND}
+        };
+    }
+
 
 
 }
