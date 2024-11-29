27e25e6abba933037812ad6158c1303e35678f07
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-11-29 12:08:27 +0100

    added a new test for expansion

--- src/test/java/hr/bp/adventofcode/day11/UniverseTest.java
+++ src/test/java/hr/bp/adventofcode/day11/UniverseTest.java
@@ -3,8 +3,11 @@ package hr.bp.adventofcode.day11;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static hr.bp.adventofcode.day11.TestUtils.getFirstImageExpanded;
 import static hr.bp.adventofcode.day11.TestUtils.getFirstInput;
 import static hr.bp.adventofcode.day11.TestUtils.getFirstImage;
+import static hr.bp.adventofcode.day11.TestUtils.getSecondImageExpanded;
+import static hr.bp.adventofcode.day11.TestUtils.getSecondInput;
 
 /**
  * @author Ivan Tomičić
@@ -21,5 +24,31 @@ public class UniverseTest {
         char[][] actualImage = universe.getImage();
 
         Assertions.assertArrayEquals(expectedImage, actualImage);
+    }
+
+    @Test
+    public void expandUniverse_givenValidInputOne_correctlyExpandsUniverse() {
+        String input = getFirstInput();
+        Universe universe = new Universe(input);
+        universe.expand();
+
+        char[][] expectedImageAfterExpansion = getFirstImageExpanded();
+
+        char[][] actualImageAfterExpansion = universe.getImage();
+
+        Assertions.assertArrayEquals(expectedImageAfterExpansion, actualImageAfterExpansion);
+    }
+
+    @Test
+    public void expandUniverse_givenValidInputTwo_correctlyExpandsUniverse() {
+        String input = getSecondInput();
+        Universe universe = new Universe(input);
+        universe.expand();
+
+        char[][] expectedImageAfterExpansion = getSecondImageExpanded();
+
+        char[][] actualImageAfterExpansion = universe.getImage();
+
+        Assertions.assertArrayEquals(expectedImageAfterExpansion, actualImageAfterExpansion);
     }
 }
