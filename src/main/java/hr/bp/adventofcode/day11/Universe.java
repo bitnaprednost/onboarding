5a35ed76eaa4bbf5e82aa588e542543ee9828d7d
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-11-29 12:08:33 +0100

    implemented expand method

--- src/main/java/hr/bp/adventofcode/day11/Universe.java
+++ src/main/java/hr/bp/adventofcode/day11/Universe.java
@@ -1,5 +1,8 @@
 package hr.bp.adventofcode.day11;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Ivan Tomičić
  */
@@ -25,5 +28,83 @@ public class Universe {
 
     public char[][] getImage() {
         return image;
+    }
+
+    public void expand() {
+        List<Integer> emptyRowIndexes = getEmptyRowIndexes();
+        List<Integer> emptyColumnIndexes = getEmptyColumnIndexes();
+
+        char[][] newImage = new char[image.length + emptyRowIndexes.size()][image[0].length + emptyColumnIndexes.size()];
+        expandRowsAndThenColumns(newImage, emptyRowIndexes, emptyColumnIndexes);
+    }
+
+    private void expandRowsAndThenColumns(char[][] newImage, List<Integer> emptyRowIndexes, List<Integer> emptyColumnIndexes) {
+        expandRows(newImage, emptyRowIndexes);
+        expandColumns(newImage, emptyColumnIndexes);
+    }
+
+    public void expandColumns(char[][] original, List<Integer> indicesToDuplicate) {
+        int originalRows = original.length;
+        int originalCols = original[0].length;
+
+        char[][] expanded = new char[originalRows][originalCols];
+
+        for (int i = 0; i < originalRows; i++) {
+            int newColIndex = 0;
+            for (int j = 0; j < originalCols; j++) {
+                if (newColIndex + 1 > originalCols) break;
+                expanded[i][newColIndex++] = original[i][j];
+
+                for (int index : indicesToDuplicate) {
+                    if (j == index) {
+                        expanded[i][newColIndex++] = original[i][j];
+                        break;
+                    }
+                }
+            }
+        }
+        image = expanded;
+    }
+
+    private void expandRows(char[][] newImage, List<Integer> emptyRowIndexes) {
+        int duplicatedRows = 0;
+        for (int row = 0; row < image.length; row++) {
+            System.arraycopy(image[row], 0, newImage[row + duplicatedRows], 0, image[row].length);
+            if (emptyRowIndexes.contains(row)) {
+                System.arraycopy(image[row], 0, newImage[row + ++duplicatedRows], 0, image[row].length);
+            }
+        }
+    }
+
+    private List<Integer> getEmptyColumnIndexes() {
+        List<Integer> emptyColumnIndexes = new ArrayList<>();
+
+        for (int column = 0; column < image[0].length; column++) {
+            boolean columnIsEmpty = true;
+            for (int row = 0; row < image.length; row++) {
+                if (image[row][column] == '#') {
+                    columnIsEmpty = false;
+                    break;
+                }
+            }
+            if (columnIsEmpty) emptyColumnIndexes.add(column);
+        }
+        return emptyColumnIndexes;
+    }
+
+    private List<Integer> getEmptyRowIndexes() {
+        List<Integer> emptyRowIndexes = new ArrayList<>();
+
+        for (int row = 0; row < image.length; row++) {
+            boolean rowIsEmpty = true;
+            for (int column = 0; column < image[row].length; column++) {
+                if (image[row][column] == '#') {
+                    rowIsEmpty = false;
+                    break;
+                }
+            }
+            if (rowIsEmpty) emptyRowIndexes.add(row);
+        }
+        return emptyRowIndexes;
     }
 }
