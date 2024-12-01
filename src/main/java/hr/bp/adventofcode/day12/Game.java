8a4009ec3a3f4081582f55d53ffab33ee0b09640
itomicic <ivan.tomicic@outlook.com>
2024-12-01 16:54:41 +0100

    removed unused imports and comments

--- src/main/java/hr/bp/adventofcode/day12/Game.java
+++ src/main/java/hr/bp/adventofcode/day12/Game.java
@@ -2,9 +2,7 @@ package hr.bp.adventofcode.day12;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author Ivan Tomičić
--- src/main/java/hr/bp/adventofcode/day12/SpringRow.java
+++ src/main/java/hr/bp/adventofcode/day12/SpringRow.java
@@ -1,8 +1,5 @@
 package hr.bp.adventofcode.day12;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,8 +15,6 @@ public class SpringRow {
 
     private final List<Integer> contiguousGroups;
 
-    private List<Integer> differentArrangementsCalculated;
-
     public SpringRow(String springs, List<Integer> contiguousGroups) {
         this.springs = springs;
         this.contiguousGroups = contiguousGroups;
@@ -27,47 +22,39 @@ public class SpringRow {
     }
 
     public static long countWays(String input, List<Integer> groups) {
-        // Create cache for dynamic programming
         Map<String, Long> cache = new HashMap<>();
         return countWaysHelper(input, 0, groups, 0, cache);
     }
 
     private static long countWaysHelper(String input, int pos, List<Integer> groups, int groupIndex, Map<String, Long> cache) {
-        // Create cache key
         String key = pos + "," + groupIndex;
         if (cache.containsKey(key)) {
             return cache.get(key);
         }
 
-        // Base cases
         if (groupIndex == groups.size()) {
-            // Check if there are any remaining hash signs
             for (int i = pos; i < input.length(); i++) {
                 if (input.charAt(i) == '#') {
-                    return 0; // Invalid case: uncovered hash sign
+                    return 0;
                 }
             }
-            return 1; // Valid solution found
+            return 1;
         }
 
         if (pos >= input.length()) {
-            return 0; // Reached end of string but groups remain
+            return 0;
         }
 
         long result = 0;
         int currentGroup = groups.get(groupIndex);
 
-        // Try placing the current group starting at pos
         if (canPlaceGroup(input, pos, currentGroup)) {
-            // If we can place the group, we need to ensure there's a dot or question mark after it
-            // (unless it's at the end of the string)
             if (pos + currentGroup == input.length() ||
                     input.charAt(pos + currentGroup) != '#') {
                 result += countWaysHelper(input, pos + currentGroup + 1, groups, groupIndex + 1, cache);
             }
         }
 
-        // If current position is not a hash sign, we can skip it
         if (input.charAt(pos) != '#') {
             result += countWaysHelper(input, pos + 1, groups, groupIndex, cache);
         }
@@ -77,12 +64,10 @@ public class SpringRow {
     }
 
     private static boolean canPlaceGroup(String input, int pos, int groupSize) {
-        // Check if there's enough space
         if (pos + groupSize > input.length()) {
             return false;
         }
 
-        // Check if all positions can be part of the group
         for (int i = 0; i < groupSize; i++) {
             char c = input.charAt(pos + i);
             if (c == '.') {
