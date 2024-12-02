d03cb1098d104c5324c14d1a06e435ba32b6525a
marijahorvat171 <marijahorvat171@gmail.com>
2024-12-02 16:09:37 +0100

    git - modify classes

--- src/main/java/aoc_2024/day1/SimilarityScore.java
+++ src/main/java/aoc_2024/day1/SimilarityScore.java
@@ -1,26 +1,12 @@
 package aoc_2024.day1;
 
-import java.util.ArrayList;
 import java.util.Collections;
 
-public class SimilarityScore {
+public class SimilarityScore extends TotalDistance {
     public int calculateSimilarityScore (String locationIDs){
         int totalSum = 0;
 
-        // Split the file content into lines
-        String[] lines = locationIDs.split(System.lineSeparator());
-
-        ArrayList<Integer> left = new ArrayList<Integer>();
-        ArrayList<Integer> right = new ArrayList<Integer>();
-
-        for (String line : lines)
-        {
-            String[] parts = line.split("\\s+");
-
-            left.add(Integer.parseInt(parts[0].trim()));
-            right.add(Integer.parseInt(parts[1].trim()));
-        }
-
+        setLists(locationIDs);
 
         for (int i = 0; i < left.size(); ++i)
         {
