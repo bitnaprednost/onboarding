cf4b2e090d40db531e7cfb39c6cf034507be98b0
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-12-02 16:49:26 +0100

    added a record and enum class for representing symmetry

---
+++ src/main/java/hr/bp/adventofcode/day13/SymmetryDirection.java
@@ -0,0 +1,8 @@
+package hr.bp.adventofcode.day13;
+
+/**
+ * @author Ivan Tomičić
+ */
+public enum SymmetryDirection {
+    COLUMN, ROW
+}
---
+++ src/main/java/hr/bp/adventofcode/day13/SymmetryType.java
@@ -0,0 +1,7 @@
+package hr.bp.adventofcode.day13;
+
+/**
+ * @author Ivan Tomičić
+ */
+public record SymmetryType (int index, SymmetryDirection direction) {
+}
