5aa6a9e0b53fa5e3db92809d9093d8eed1e32a99
itomicic <ivan.tomicic@outlook.com>
2024-12-03 19:49:16 +0100

    added a test for confirmed correct solution of task 1

--- src/test/java/hr/bp/adventofcode/day16/ContraptionTest.java
+++ src/test/java/hr/bp/adventofcode/day16/ContraptionTest.java
@@ -3,6 +3,7 @@ package hr.bp.adventofcode.day16;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static hr.bp.adventofcode.Utils.readInputForDay;
 import static hr.bp.adventofcode.day16.TestUtils.getFirstInput;
 
 /**
@@ -16,6 +17,18 @@ public class ContraptionTest {
         Contraption contraption = new Contraption(input);
 
         int expectedCountOfEnergizedTiles = 46;
+
+        int actualCountOfEnergizedTiles = contraption.countEnergizedTiles();
+
+        Assertions.assertEquals(expectedCountOfEnergizedTiles, actualCountOfEnergizedTiles);
+    }
+
+    @Test
+    public void countEnergizedTiles_givenTaskInput_returnsCorrectCount() {
+        String input = readInputForDay("day16");
+        Contraption contraption = new Contraption(input);
+
+        int expectedCountOfEnergizedTiles = 7472;
 
         int actualCountOfEnergizedTiles = contraption.countEnergizedTiles();
 
