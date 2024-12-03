7949270fc7eb2e26379fe732034bbd8b7ccc01fc
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-12-03 13:48:33 +0100

    added test for confirmed solution of task 1

--- src/test/java/hr/bp/adventofcode/day14/PlatformTest.java
+++ src/test/java/hr/bp/adventofcode/day14/PlatformTest.java
@@ -3,6 +3,7 @@ package hr.bp.adventofcode.day14;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static hr.bp.adventofcode.Utils.readInputForDay;
 import static hr.bp.adventofcode.day14.TestUtils.getFirstGridAfterTiltNorth;
 import static hr.bp.adventofcode.day14.TestUtils.getFirstInput;
 
@@ -32,6 +33,19 @@ public class PlatformTest {
         platform.tilt(PlatformDirection.NORTH);
 
         int expectedLoad = 136;
+
+        int actualLoad = platform.getLoad();
+
+        Assertions.assertEquals(expectedLoad, actualLoad);
+    }
+
+    @Test
+    public void calculateTotalLoad_givenTaskInput_returnsCorrectLoad() {
+        String input = readInputForDay("day14");
+        Platform platform = new Platform(input);
+        platform.tilt(PlatformDirection.NORTH);
+
+        int expectedLoad = 106990;
 
         int actualLoad = platform.getLoad();
 
