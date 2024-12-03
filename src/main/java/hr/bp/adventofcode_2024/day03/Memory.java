2943576b0dfba0143fd8c655dc26931f9a3c7dbf
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-12-03 11:29:04 +0100

    added implementation of conditions, refactored previous solution

--- src/main/java/hr/bp/adventofcode_2024/day03/Memory.java
+++ src/main/java/hr/bp/adventofcode_2024/day03/Memory.java
@@ -2,8 +2,8 @@ package hr.bp.adventofcode_2024.day03;
 
 import hr.bp.adventofcode_2024.Pair;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,47 +12,82 @@ import java.util.regex.Pattern;
  */
 public class Memory {
 
-    private final List<Pair<Integer, Integer>> multiplicationInstructions = new ArrayList<>();
+    private final Map<Integer, Pair<Integer, Integer>> multiplicationInstructions = new HashMap<>();
+
+    private final Map<Integer, Boolean> conditions = new HashMap<>();
 
     public Memory(String input) {
         parseInput(input);
     }
 
     private void parseInput(String input) {
-        for (String line : input.split("\\n")) {
-            addMultiplicationsToList(line);
+        extractMultiplications(input);
+        extractConditions(input);
+    }
+
+    private void extractConditions(String line) {
+        conditions.put(0, true);
+
+        String regex = "do\\(\\)|don't\\(\\)";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(line);
+
+        while (matcher.find()) {
+            boolean isDo = matcher.group().equals("do()");
+            conditions.put(matcher.end() - 1, isDo);
         }
     }
 
-    private void addMultiplicationsToList(String line) {
+    private void extractMultiplications(String line) {
         String regex = "mul\\((\\d+),(\\d+)\\)";
 
         Pattern pattern = Pattern.compile(regex);
 
         Matcher matcher = pattern.matcher(line);
 
-        List<String> matches = new ArrayList<>();
-
         while (matcher.find()) {
-            matches.add(matcher.group());
-        }
+            String match = matcher.group();
 
-        for (String match : matches) {
             Integer leftNumber = Integer.parseInt(match.split(",")[0].split("\\(")[1]);
             Integer rightNumber = Integer.parseInt(match.split(",")[1].split("\\)")[0]);
-            multiplicationInstructions.add(new Pair<>(leftNumber, rightNumber));
+
+            int startIndex = matcher.start();
+
+            multiplicationInstructions.put(startIndex, new Pair<>(leftNumber, rightNumber));
         }
     }
 
     public long sumMultiplications() {
         long sum = 0;
-        for (Pair<Integer, Integer> pair : multiplicationInstructions) {
+        for (Pair<Integer, Integer> pair : multiplicationInstructions.values()) {
             sum += (long) pair.left() * pair.right();
         }
         return sum;
     }
 
-    public List<Pair<Integer, Integer>> getMultiplicationInstructions() {
+    public long sumMultiplicationsWithConditions() {
+        long sum = 0;
+        for (Map.Entry<Integer, Pair<Integer, Integer>> entry : multiplicationInstructions.entrySet()) {
+            boolean condition = findNearestCondition(entry.getKey());
+            if (condition) {
+                Pair<Integer, Integer> pair = entry.getValue();
+                sum += (long) pair.left() * pair.right();
+            }
+        }
+        return sum;
+    }
+
+    private boolean findNearestCondition(int index) {
+        while (index >= 0) {
+            if (conditions.get(index) != null) return conditions.get(index);
+            index--;
+        }
+        return true;
+    }
+
+    public Map<Integer, Pair<Integer, Integer>> getMultiplicationInstructions() {
         return multiplicationInstructions;
     }
 }
