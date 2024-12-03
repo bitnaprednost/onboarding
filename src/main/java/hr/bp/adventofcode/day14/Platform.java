93f1345f2fdcb74703bab2facf800a3454482ee4
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-12-03 11:54:10 +0100

    added empty Platform class with expected methods

---
+++ src/main/java/hr/bp/adventofcode/day14/Platform.java
@@ -0,0 +1,24 @@
+package hr.bp.adventofcode.day14;
+
+/**
+ * @author Ivan Tomičić
+ */
+public class Platform {
+    
+    public Platform(String input) {
+
+    }
+
+    public void tilt(PlatformDirection platformDirection) {
+
+    }
+
+    public char[][] getGrid() {
+
+        return null;
+    }
+
+    public int getLoad() {
+        return 0;
+    }
+}
---
+++ src/main/java/hr/bp/adventofcode/day14/PlatformDirection.java
@@ -0,0 +1,8 @@
+package hr.bp.adventofcode.day14;
+
+/**
+ * @author Ivan Tomičić
+ */
+public enum PlatformDirection {
+    NORTH, SOUTH, EAST, WEST
+}
