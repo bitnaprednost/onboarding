b3b3add55627489eebbe5fee1503cad39bc642b2
Nikolina Spehar <speharnikolina87@gmail.com>
2024-12-04 08:54:58 +0100

    refactoring - replacing test input with correct string

--- src/test/java/hr/bp/aoc2024/day3/TestUtil.java
+++ src/test/java/hr/bp/aoc2024/day3/TestUtil.java
@@ -14,7 +14,7 @@ public class TestUtil {
     }
 
     public static String getTestInput() {
-        return "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        return "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64]\n(mul(11,8)undo()?mul(8,5))";
     }
 
     public static String getFinalInput() {
