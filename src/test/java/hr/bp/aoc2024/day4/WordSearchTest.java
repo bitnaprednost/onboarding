43e67f9c44128f7e06ba7a3edb059d6591bf78c2
Nikolina Spehar <speharnikolina87@gmail.com>
2024-12-04 11:17:02 +0100

    new - added test for word searches in all directions

---
+++ src/test/java/hr/bp/aoc2024/day4/WordSearchTest.java
@@ -0,0 +1,37 @@
+package hr.bp.aoc2024.day4;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static hr.bp.aoc2024.day4.TestUtil.*;
+
+class WordSearchTest {
+
+    @Test
+    void testFindAllWords_testInput() {
+        WordSearchParser parser = new WordSearchParser(getTestInput());
+        WordSearch wordSearch = new WordSearch(parser.parseWordSearchInput());
+        int expected = 18;
+
+        Assertions.assertEquals(expected, wordSearch.findWordInSearchTable());
+    }
+
+    @Test
+    void testFindAllWords_finalInput() {
+        WordSearchParser parser = new WordSearchParser(getFinalInput());
+        WordSearch wordSearch = new WordSearch(parser.parseWordSearchInput());
+        int expected = 2483;
+
+        Assertions.assertEquals(expected, wordSearch.findWordInSearchTable());
+    }
+
+    @Test
+    void testFindAllWords_allDirections() {
+        WordSearchParser parser = new WordSearchParser(getTestInput_allDirections());
+        WordSearch wordSearch = new WordSearch(parser.parseWordSearchInput());
+        int expected = 5;
+
+        Assertions.assertEquals(expected, wordSearch.findWordInSearchTable());
+    }
+
+}
