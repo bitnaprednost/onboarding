b8806e9bc51742b04be894a7885edae39e265100
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-12-05 11:13:38 +0100

    added test for confirmed correct solution of the 1st task

--- src/test/java/hr/bp/adventofcode_2024/day05/PageProcessorTest.java
+++ src/test/java/hr/bp/adventofcode_2024/day05/PageProcessorTest.java
@@ -3,6 +3,7 @@ package hr.bp.adventofcode_2024.day05;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static hr.bp.adventofcode_2024.Utils.readInputForDay;
 import static hr.bp.adventofcode_2024.day05.TestUtils.getFirstInput;
 
 /**
@@ -16,6 +17,18 @@ public class PageProcessorTest {
         PageProcessor pageProcessor = new PageProcessor(input);
 
         int expectedSum = 143;
+
+        int actualSum = pageProcessor.calculateSumOfMiddleNumbers();
+
+        Assertions.assertEquals(expectedSum, actualSum);
+    }
+
+    @Test
+    public void calculateSumOfMiddlePages_givenTaskInput_returnsCorrectSum() {
+        String input = readInputForDay("day05");
+        PageProcessor pageProcessor = new PageProcessor(input);
+
+        int expectedSum = 4996;
 
         int actualSum = pageProcessor.calculateSumOfMiddleNumbers();
 
