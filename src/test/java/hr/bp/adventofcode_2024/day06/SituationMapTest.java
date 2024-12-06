1b3ca048e80f9fd8ad97a76ece6d17274275cbe3
Ivan Tomicic <ivan.tomicic@outlook.com>
2024-12-06 18:08:05 +0100

    added test for confirmed correct solution of task 1

--- src/test/java/hr/bp/adventofcode_2024/day06/SituationMapTest.java
+++ src/test/java/hr/bp/adventofcode_2024/day06/SituationMapTest.java
@@ -3,6 +3,7 @@ package hr.bp.adventofcode_2024.day06;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static hr.bp.adventofcode_2024.Utils.readInputForDay;
 import static hr.bp.adventofcode_2024.day06.TestUtils.getFirstInput;
 
 /**
@@ -16,6 +17,18 @@ public class SituationMapTest {
         SituationMap situationMap = new SituationMap(input);
 
         int expectedDistinctPositions = 41;
+
+        int actualDistinctPositions = situationMap.countDistinctPositions();
+
+        Assertions.assertEquals(expectedDistinctPositions, actualDistinctPositions);
+    }
+
+    @Test
+    public void calculateDistinctPositions_givenTaskInput_returnsCorrectValue() {
+        String input = readInputForDay("day06");
+        SituationMap situationMap = new SituationMap(input);
+
+        int expectedDistinctPositions = 5145;
 
         int actualDistinctPositions = situationMap.countDistinctPositions();
 
