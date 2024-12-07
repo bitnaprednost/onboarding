5334468829e17771b99735e04fca9d4116f8729b
itomicic <ivan.tomicic@outlook.com>
2024-12-07 22:58:25 +0100

    added 2 tests for confirmed correct solutions for task 1 and 2

--- src/test/java/hr/bp/adventofcode_2024/day07/EquationRunnerTest.java
+++ src/test/java/hr/bp/adventofcode_2024/day07/EquationRunnerTest.java
@@ -3,6 +3,7 @@ package hr.bp.adventofcode_2024.day07;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static hr.bp.adventofcode_2024.Utils.readInputForDay;
 import static hr.bp.adventofcode_2024.day07.TestUtils.getFirstInput;
 
 /**
@@ -23,11 +24,35 @@ public class EquationRunnerTest {
     }
 
     @Test
+    public void findIfEquationHasSolution_givenTaskInput_returnsCorrectResult() {
+        String input = readInputForDay("day07");
+        EquationRunner equationRunner = new EquationRunner(input);
+
+        long expectedSum = 3598800864292L;
+
+        long actualSum = equationRunner.sumSolvableEquations();
+
+        Assertions.assertEquals(expectedSum, actualSum);
+    }
+
+    @Test
     public void findIfEquationHasSolutionWithAdditionalOperator_givenInputOne_returnsCorrectResult() {
         String input = getFirstInput();
         EquationRunner equationRunner = new EquationRunner(input);
 
         long expectedSum = 11387;
+
+        long actualSum = equationRunner.sumSolvableEquationsWithAdditionalOperator();
+
+        Assertions.assertEquals(expectedSum, actualSum);
+    }
+
+    @Test
+    public void findIfEquationHasSolutionWithAdditionalOperator_givenTaskInput_returnsCorrectResult() {
+        String input = readInputForDay("day07");
+        EquationRunner equationRunner = new EquationRunner(input);
+
+        long expectedSum = 340362529351427L;
 
         long actualSum = equationRunner.sumSolvableEquationsWithAdditionalOperator();
 
