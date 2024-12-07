8bd6ba9efbf2f9456b049b43389d0d49c36f9f88
itomicic <ivan.tomicic@outlook.com>
2024-12-07 22:02:51 +0100

    added more tests

--- src/test/java/hr/bp/adventofcode_2024/day07/EquationSolverTest.java
+++ src/test/java/hr/bp/adventofcode_2024/day07/EquationSolverTest.java
@@ -14,9 +14,80 @@ public class EquationSolverTest {
     public void findIfEquationHasSolution_givenInputOne_returnsCorrectResult() {
         long value = 190;
         List<Long> numbers = List.of(19L, 10L);
+
         EquationSolver equationSolver = new EquationSolver(value, numbers);
 
         boolean expectedEquationHasSolution = true;
+
+        boolean actualEquationHasSolution = equationSolver.findSolution();
+
+        Assertions.assertEquals(expectedEquationHasSolution, actualEquationHasSolution);
+    }
+
+    @Test
+    public void findIfEquationHasSolution_givenInputTwo_returnsCorrectResult() {
+        long value = 3267;
+        List<Long> numbers = List.of(81L, 40L, 27L);
+
+        EquationSolver equationSolver = new EquationSolver(value, numbers);
+
+        boolean expectedEquationHasSolution = true;
+
+        boolean actualEquationHasSolution = equationSolver.findSolution();
+
+        Assertions.assertEquals(expectedEquationHasSolution, actualEquationHasSolution);
+    }
+
+    @Test
+    public void findIfEquationHasSolution_givenInputThree_returnsCorrectResult() {
+        long value = 292;
+        List<Long> numbers = List.of(11L, 6L, 16L, 20L);
+
+        EquationSolver equationSolver = new EquationSolver(value, numbers);
+
+        boolean expectedEquationHasSolution = true;
+
+        boolean actualEquationHasSolution = equationSolver.findSolution();
+
+        Assertions.assertEquals(expectedEquationHasSolution, actualEquationHasSolution);
+    }
+
+    @Test
+    public void findIfEquationHasSolution_givenInputFour_returnsCorrectResult() {
+        long value = 161011;
+        List<Long> numbers = List.of(16L, 10L, 13L);
+
+        EquationSolver equationSolver = new EquationSolver(value, numbers);
+
+        boolean expectedEquationHasSolution = false;
+
+        boolean actualEquationHasSolution = equationSolver.findSolution();
+
+        Assertions.assertEquals(expectedEquationHasSolution, actualEquationHasSolution);
+    }
+
+    @Test
+    public void findIfEquationHasSolution_givenInputFive_returnsCorrectResult() {
+        long value = 192;
+        List<Long> numbers = List.of(17L, 8L,14L);
+
+        EquationSolver equationSolver = new EquationSolver(value, numbers);
+
+        boolean expectedEquationHasSolution = false;
+
+        boolean actualEquationHasSolution = equationSolver.findSolution();
+
+        Assertions.assertEquals(expectedEquationHasSolution, actualEquationHasSolution);
+    }
+
+    @Test
+    public void findIfEquationHasSolution_givenInputSix_returnsCorrectResult() {
+        long value = 83;
+        List<Long> numbers = List.of(17L, 5L);
+
+        EquationSolver equationSolver = new EquationSolver(value, numbers);
+
+        boolean expectedEquationHasSolution = false;
 
         boolean actualEquationHasSolution = equationSolver.findSolution();
 
