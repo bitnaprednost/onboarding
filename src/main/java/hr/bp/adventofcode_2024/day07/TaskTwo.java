8c7ccc1af71f110f94db75a33c0bbc94f98d9a93
itomicic <ivan.tomicic@outlook.com>
2024-12-07 22:56:53 +0100

    added class for running the 2nd task

---
+++ src/main/java/hr/bp/adventofcode_2024/day07/TaskTwo.java
@@ -0,0 +1,18 @@
+package hr.bp.adventofcode_2024.day07;
+
+import static hr.bp.adventofcode_2024.Utils.displayResult;
+import static hr.bp.adventofcode_2024.Utils.readInputForDay;
+
+/**
+ * @author Ivan Tomičić
+ */
+public class TaskTwo {
+
+    public static void main(String[] args) {
+        String input = readInputForDay("day07");
+
+        EquationRunner equationRunner = new EquationRunner(input);
+
+        displayResult(equationRunner.sumSolvableEquationsWithAdditionalOperator());
+    }
+}
