cfdea61eab7357e7e0db26577a9ac3bd8189ddb2
itomicic <ivan.tomicic@outlook.com>
2024-12-08 19:20:25 +0100

    added test for confirmed solution for 1st task

--- src/test/java/hr/bp/adventofcode_2024/day08/AntennaGridTest.java
+++ src/test/java/hr/bp/adventofcode_2024/day08/AntennaGridTest.java
@@ -3,6 +3,7 @@ package hr.bp.adventofcode_2024.day08;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static hr.bp.adventofcode_2024.Utils.readInputForDay;
 import static hr.bp.adventofcode_2024.day08.TestUtils.getFirstInput;
 
 /**
@@ -11,11 +12,23 @@ import static hr.bp.adventofcode_2024.day08.TestUtils.getFirstInput;
 public class AntennaGridTest {
 
     @Test
-    public void findAntinodes_givenInput_returnsCorrectResult() {
+    public void findAntinodes_givenInputOne_returnsCorrectResult() {
         String input = getFirstInput();
         AntennaGrid antennaGrid = new AntennaGrid(input);
 
         int expectedUniqueAntinodes = 14;
+
+        int actualUniqueAntinodes = antennaGrid.findUniqueAntinodes();
+
+        Assertions.assertEquals(expectedUniqueAntinodes, actualUniqueAntinodes);
+    }
+
+    @Test
+    public void findAntinodes_givenTaskInput_returnsCorrectResult() {
+        String input = readInputForDay("day08");
+        AntennaGrid antennaGrid = new AntennaGrid(input);
+
+        int expectedUniqueAntinodes = 371;
 
         int actualUniqueAntinodes = antennaGrid.findUniqueAntinodes();
 
