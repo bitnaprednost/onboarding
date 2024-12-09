e58dffbbf5e2aeda575a6eb42d1de4c894633895
Nikolina Spehar <speharnikolina87@gmail.com>
2024-12-09 12:52:18 +0100

    new - adding tests for task 1

---
+++ src/test/java/hr/bp/aoc2024/day9/DiskTest.java
@@ -0,0 +1,31 @@
+package hr.bp.aoc2024.day9;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static hr.bp.aoc2024.day9.Util.getFinalInput;
+import static hr.bp.aoc2024.day9.Util.getTestInput;
+
+class DiskTest {
+    private static Disk disk;
+
+    @Test
+    void getFileSystemChecksum_testInput() {
+        long expected = 1928;
+
+        DiskParser diskParser = new DiskParser(getTestInput());
+        disk = new Disk(diskParser);
+
+        Assertions.assertEquals(expected, disk.getFileSystemChecksum());
+    }
+
+    @Test
+    void getFileSystemChecksum_finalInput() {
+        DiskParser diskParser = new DiskParser(getFinalInput());
+        disk = new Disk(diskParser);
+
+        long expectedValue = 6241633730082L;
+
+        Assertions.assertEquals(expectedValue, disk.getFileSystemChecksum());
+    }
+}
