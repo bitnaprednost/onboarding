cee010135eddd4e86f4df215bfc35465b32c0405
Nikolina Spehar <speharnikolina87@gmail.com>
2024-12-10 11:01:53 +0100

    new - helper class Coordinate

---
+++ src/main/java/hr/bp/aoc2024/day10/Coordinate.java
@@ -0,0 +1,28 @@
+package hr.bp.aoc2024.day10;
+
+import java.util.Objects;
+
+public class Coordinate {
+    private int x;
+    private int y;
+
+    public Coordinate(int y, int x) {
+        this.y = y;
+        this.x = x;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Coordinate that = (Coordinate) o;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
