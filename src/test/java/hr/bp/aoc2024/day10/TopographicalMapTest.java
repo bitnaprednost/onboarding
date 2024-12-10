3066b79d64ec87e9007dc4a0598e370e7f4b364d
Nikolina Spehar <speharnikolina87@gmail.com>
2024-12-10 11:00:53 +0100

    new - added tests for TopographicalMap

---
+++ src/test/java/hr/bp/aoc2024/day10/TopographicalMapTest.java
@@ -0,0 +1,49 @@
+package hr.bp.aoc2024.day10;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static hr.bp.aoc2024.day10.TestUtil.*;
+
+class TopographicalMapTest {
+
+    @Test
+    void getTrailheadScore_twoTrailheads() {
+        TopographicMapParser parser = new TopographicMapParser(getTestInput());
+        TopographicalMap map = new TopographicalMap(parser);
+
+        Assertions.assertEquals(2, map.calculateTrailheadScore());
+    }
+
+    @Test
+    void getTrailheadScore_fourTrailheads() {
+        TopographicMapParser parser = new TopographicMapParser(getFourTrailheadsInput());
+        TopographicalMap map = new TopographicalMap(parser);
+
+        Assertions.assertEquals(4, map.calculateTrailheadScore());
+    }
+
+    @Test
+    void getTrailheadScore_threeTrailheads() {
+        TopographicMapParser parser = new TopographicMapParser(getThreeTrailheadsInput());
+        TopographicalMap map = new TopographicalMap(parser);
+
+        Assertions.assertEquals(3, map.calculateTrailheadScore());
+    }
+
+    @Test
+    void getTrailheadScore_finalTestInputTrailheads() {
+        TopographicMapParser parser = new TopographicMapParser(getFinalTestTrailheadsInput());
+        TopographicalMap map = new TopographicalMap(parser);
+
+        Assertions.assertEquals(36, map.calculateTrailheadScore());
+    }
+
+    @Test
+    void getTrailheadScore_finalInputTrailheads() {
+        TopographicMapParser parser = new TopographicMapParser(getFinalTrailheadsInput());
+        TopographicalMap map = new TopographicalMap(parser);
+
+        Assertions.assertEquals(550, map.calculateTrailheadScore());
+    }
+}
