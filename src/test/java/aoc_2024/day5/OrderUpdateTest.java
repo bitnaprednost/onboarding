81dbb6c8a8737834a75f0d7be97f7b75e10b7ef6
marijahorvat171 <marijahorvat171@gmail.com>
2024-12-11 12:22:08 +0100

    git - add tests for aoc day5

---
+++ src/test/java/aoc_2024/day5/OrderUpdateTest.java
@@ -0,0 +1,67 @@
+package aoc_2024.day5;
+
+import io.ReadFile;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderUpdateTest {
+
+    PageOrderingRules rules = new PageOrderingRules();
+
+    @Test
+    void getValidUpdatesMiddleSum_example() {
+
+        String input = """
+                47|53
+                97|13
+                97|61
+                97|47
+                75|29
+                61|13
+                75|53
+                29|13
+                97|29
+                53|29
+                61|53
+                97|53
+                61|29
+                47|13
+                75|47
+                97|75
+                47|61
+                75|61
+                47|29
+                75|13
+                53|13
+                
+                75,47,61,53,29
+                97,61,53,29,13
+                75,29,13
+                75,97,47,61,53
+                61,13,29
+                97,13,75,29,47
+                """;
+
+        rules.orderingRules(input);
+
+        OrderUpdate validator = new OrderUpdate(rules.getRules(), rules.getPrintPages());
+        assertEquals(123, validator.getValidUpdatesMiddleSum());
+    }
+
+    @Test
+    void getValidUpdatesMiddleSum_input_puzzle(){
+
+        // Specify the file name
+        String fileName = "src/main/resources/aoc_2024/day5/Pages.txt";
+
+        // Read the file
+        ReadFile readFile = new ReadFile();
+        String fileContent = readFile.readFile(fileName);
+
+        rules.orderingRules(fileContent);
+
+        OrderUpdate validator = new OrderUpdate(rules.getRules(), rules.getPrintPages());
+        assertEquals(6456, validator.getValidUpdatesMiddleSum());
+    }
+}
