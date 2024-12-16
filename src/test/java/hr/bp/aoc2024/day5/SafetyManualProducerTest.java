f4d3e8811d25b350463649fbe976794e70f4a929
Nikolina Spehar <speharnikolina87@gmail.com>
2024-12-16 12:54:29 +0100

    refactoring - added tests for correcting printing order

--- src/test/java/hr/bp/aoc2024/day5/SafetyManualProducerTest.java
+++ src/test/java/hr/bp/aoc2024/day5/SafetyManualProducerTest.java
@@ -30,4 +30,26 @@ class SafetyManualProducerTest {
 
         Assertions.assertEquals(expected, safetyManualProducer.getMiddlePageSum());
     }
+
+    @Test
+    void testInput_middleSumCorrectedOrder() {
+        PageParser pageParser = new PageParser(getTestInput());
+        SafetyManualProducer safetyManualProducer = new SafetyManualProducer(pageParser.getOrderingRules(),
+                pageParser.getProducingOrder());
+
+        long expected = 123;
+
+        Assertions.assertEquals(expected, safetyManualProducer.getMiddlePageSumIncorrectOrder());
+    }
+
+    @Test
+    void finalInput_middleSumCorrectedOrder() {
+        PageParser pageParser = new PageParser(getFinalInput());
+        SafetyManualProducer safetyManualProducer = new SafetyManualProducer(pageParser.getOrderingRules(),
+                pageParser.getProducingOrder());
+
+        long expected = 4944;
+
+        Assertions.assertEquals(expected, safetyManualProducer.getMiddlePageSumIncorrectOrder());
+    }
 }
