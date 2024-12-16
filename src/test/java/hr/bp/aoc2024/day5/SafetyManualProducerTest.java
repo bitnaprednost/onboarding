173c8453af21673a3ccc565d653c73fe3e4505cc
Nikolina Spehar <speharnikolina87@gmail.com>
2024-12-16 11:49:29 +0100

    new - added tests

---
+++ src/test/java/hr/bp/aoc2024/day5/SafetyManualProducerTest.java
@@ -0,0 +1,33 @@
+package hr.bp.aoc2024.day5;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static hr.bp.aoc2024.day5.TestUtil.getFinalInput;
+import static hr.bp.aoc2024.day5.TestUtil.getTestInput;
+
+class SafetyManualProducerTest {
+
+    @Test
+    void testInput_middleSum() {
+        PageParser pageParser = new PageParser(getTestInput());
+        SafetyManualProducer safetyManualProducer = new SafetyManualProducer(pageParser.getOrderingRules(),
+                pageParser.getProducingOrder());
+
+        long expected = 143;
+
+        Assertions.assertEquals(expected, safetyManualProducer.getMiddlePageSum());
+    }
+
+    @Test
+    void finalInput_middleSum() {
+        PageParser pageParser = new PageParser(getFinalInput());
+        SafetyManualProducer safetyManualProducer = new SafetyManualProducer(pageParser.getOrderingRules(),
+                pageParser.getProducingOrder());
+
+        long expected = 6612;
+
+        Assertions.assertEquals(expected, safetyManualProducer.getMiddlePageSum());
+    }
+}
