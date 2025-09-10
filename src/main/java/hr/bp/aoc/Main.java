6293f7e13c707d0953e726460b016f758c60ea57
Karlo Cehulic <karlocehlic@gmail.com>
2025-09-11 01:20:21 +0200

    Display day1 solution

--- src/main/java/hr/bp/aoc/Main.java
+++ src/main/java/hr/bp/aoc/Main.java
@@ -1,6 +1,13 @@
 package hr.bp.aoc;
 
+import java.io.FileNotFoundException;
+
+import hr.bp.aoc.day1.Day1;
+
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException {
+    Day1 day1Solution = new Day1();
+
+    System.out.println("Solution is: " + day1Solution.getMaxCalories());
   }
 }
