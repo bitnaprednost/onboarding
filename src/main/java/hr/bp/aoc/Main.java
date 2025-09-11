7faded186bff3763ab46481975520cd73c6b54a8
Karlo Cehulic <karlocehlic@gmail.com>
2025-09-11 19:02:17 +0200

    Invoke day 2 solution in Main

--- src/main/java/hr/bp/aoc/Main.java
+++ src/main/java/hr/bp/aoc/Main.java
@@ -4,12 +4,18 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import hr.bp.aoc.day1.Day1;
+import hr.bp.aoc.day2.Day2;
 
 public class Main {
   public static void main(String[] args) throws FileNotFoundException, IOException {
     Day1 day1Solution = new Day1();
+    Day2 day2Solution = new Day2();
 
+    System.out.println("---DAY 1---");
     System.out.println("Solution is: " + day1Solution.getMaxCalories());
     System.out.println("Part two solution is: " + day1Solution.getThreeTotalMaxCalories());
+
+    System.out.println("---DAY 2---");
+    System.out.println("Solution is: " + day2Solution.getStrategyGuideScore());
   }
 }
