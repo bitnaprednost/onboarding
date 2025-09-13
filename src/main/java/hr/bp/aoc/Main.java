0d34759321fbd798d44317a6b45597ebbbdd7846
Karlo Cehulic <karlocehlic@gmail.com>
2025-09-13 02:49:05 +0200

    Display day 3 solution

--- src/main/java/hr/bp/aoc/Main.java
+++ src/main/java/hr/bp/aoc/Main.java
@@ -5,11 +5,13 @@ import java.io.IOException;
 
 import hr.bp.aoc.day1.Day1;
 import hr.bp.aoc.day2.Day2;
+import hr.bp.aoc.day3.Day3;
 
 public class Main {
   public static void main(String[] args) throws FileNotFoundException, IOException {
     Day1 day1Solution = new Day1();
     Day2 day2Solution = new Day2();
+    Day3 day3Solution = new Day3();
 
     System.out.println("---DAY 1---");
     System.out.println("Solution is: " + day1Solution.getMaxCalories());
@@ -18,5 +20,9 @@ public class Main {
     System.out.println("---DAY 2---");
     System.out.println("Solution is: " + day2Solution.getStrategyGuideScore());
     System.out.println("Part two solution is: " + day2Solution.getRightStrategyGuideScore());
+
+    System.out.println("---DAY 3---");
+    System.out.println("Solution is: " + day3Solution.getBothCompartmentsPrioritySum());
+
   }
 }
