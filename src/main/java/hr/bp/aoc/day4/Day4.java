1c85e4b542e38ec64b280a0b2d8d1fdad021b36a
Karlo Cehulic <karlocehlic@gmail.com>
2025-09-15 17:25:48 +0200

    Add structure to Day4 class

---
+++ src/main/java/hr/bp/aoc/day4/Day4.java
@@ -0,0 +1,27 @@
+package hr.bp.aoc.day4;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import hr.bp.aoc.DaySolution;
+
+public class Day4 extends DaySolution {
+  public Day4() throws FileNotFoundException, IOException {
+    super();
+  }
+
+  @Override
+  public String getInputFileName() {
+    return "section-assignments.txt";
+  }
+
+  @Override
+  public int getPart1Solution() {
+    return 0;
+  }
+
+  @Override
+  public int getPart2Solution() {
+    return 0;
+  }
+}
