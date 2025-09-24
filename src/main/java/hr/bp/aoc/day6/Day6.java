53e959ad9e36145d7f902f6d292fa8ad13b8d6d4
Karlo Cehulic <karlocehlic@gmail.com>
2025-09-24 21:35:18 +0200

    refactor: Move algorithm to separate method

--- src/main/java/hr/bp/aoc/day6/Day6.java
+++ src/main/java/hr/bp/aoc/day6/Day6.java
@@ -19,35 +19,41 @@ public class Day6 extends DaySolution<Integer, Integer> {
 
   @Override
   public Integer getPart1Solution() {
-    String buffer = getInputLines().get(0);
-    int NEEDED_CONSECUTIVE_CHARACTERS = 4;
-    Map<Character, Integer> lastViewedChars = new HashMap<>();
-
-    for (int i = 0; i < NEEDED_CONSECUTIVE_CHARACTERS; i++) {
-      Character currChar = buffer.charAt(i);
-      lastViewedChars.put(currChar, lastViewedChars.getOrDefault(currChar, 0) + 1);
-    }
-
-    if (lastViewedChars.size() == NEEDED_CONSECUTIVE_CHARACTERS)
-      return NEEDED_CONSECUTIVE_CHARACTERS;
-
-    for (int j = NEEDED_CONSECUTIVE_CHARACTERS; j < buffer.length(); j++) {
-      Character charToRemove = buffer.charAt(j - NEEDED_CONSECUTIVE_CHARACTERS);
-      lastViewedChars.put(charToRemove, lastViewedChars.get(charToRemove) - 1);
-      if (lastViewedChars.get(charToRemove) == 0)
-        lastViewedChars.remove(charToRemove);
-
-      Character charToAdd = buffer.charAt(j);
-      lastViewedChars.put(charToAdd, lastViewedChars.getOrDefault(charToAdd, 0) + 1);
-      if (lastViewedChars.size() == NEEDED_CONSECUTIVE_CHARACTERS)
-        return j + 1;
-    }
-
-    return -1;
+    return findFirstStartOfThePacket();
   }
 
   @Override
   public Integer getPart2Solution() {
     return 0;
+  }
+
+  private int findFirstStartOfThePacket() {
+    String buffer = getInputLines().get(0);
+    int NEEDED_CONSECUTIVE_CHARACTERS = 4;
+    Map<Character, Integer> currPreMarkerChars = new HashMap<>();
+
+    // Prerequisiste (makes main algorithm easier to write)
+    for (int i = 0; i < NEEDED_CONSECUTIVE_CHARACTERS; i++) {
+      Character currChar = buffer.charAt(i);
+      currPreMarkerChars.put(currChar, currPreMarkerChars.getOrDefault(currChar, 0) + 1);
+    }
+
+    // Earliest edge case
+    if (currPreMarkerChars.size() == NEEDED_CONSECUTIVE_CHARACTERS)
+      return NEEDED_CONSECUTIVE_CHARACTERS;
+
+    for (int j = NEEDED_CONSECUTIVE_CHARACTERS; j < buffer.length(); j++) {
+      Character charToRemove = buffer.charAt(j - NEEDED_CONSECUTIVE_CHARACTERS);
+      currPreMarkerChars.put(charToRemove, currPreMarkerChars.get(charToRemove) - 1);
+      if (currPreMarkerChars.get(charToRemove) == 0)
+        currPreMarkerChars.remove(charToRemove);
+
+      Character charToAdd = buffer.charAt(j);
+      currPreMarkerChars.put(charToAdd, currPreMarkerChars.getOrDefault(charToAdd, 0) + 1);
+      if (currPreMarkerChars.size() == NEEDED_CONSECUTIVE_CHARACTERS)
+        return j + 1;
+    }
+
+    return -1;
   }
 }
