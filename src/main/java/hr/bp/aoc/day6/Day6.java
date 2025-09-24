b4a812e0d75b864b9d11c741d32b1ea0a3d2ffd9
Karlo Cehulic <karlocehlic@gmail.com>
2025-09-24 21:28:59 +0200

    Add day 6 part 1 solution

--- src/main/java/hr/bp/aoc/day6/Day6.java
+++ src/main/java/hr/bp/aoc/day6/Day6.java
@@ -1,6 +1,8 @@
 package hr.bp.aoc.day6;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import hr.bp.aoc.DaySolution;
 
@@ -17,7 +19,31 @@ public class Day6 extends DaySolution<Integer, Integer> {
 
   @Override
   public Integer getPart1Solution() {
-    return 0;
+    String buffer = getInputLines().get(0);
+    int NEEDED_CONSECUTIVE_CHARACTERS = 4;
+    Map<Character, Integer> lastViewedChars = new HashMap<>();
+
+    for (int i = 0; i < NEEDED_CONSECUTIVE_CHARACTERS; i++) {
+      Character currChar = buffer.charAt(i);
+      lastViewedChars.put(currChar, lastViewedChars.getOrDefault(currChar, 0) + 1);
+    }
+
+    if (lastViewedChars.size() == NEEDED_CONSECUTIVE_CHARACTERS)
+      return NEEDED_CONSECUTIVE_CHARACTERS;
+
+    for (int j = NEEDED_CONSECUTIVE_CHARACTERS; j < buffer.length(); j++) {
+      Character charToRemove = buffer.charAt(j - NEEDED_CONSECUTIVE_CHARACTERS);
+      lastViewedChars.put(charToRemove, lastViewedChars.get(charToRemove) - 1);
+      if (lastViewedChars.get(charToRemove) == 0)
+        lastViewedChars.remove(charToRemove);
+
+      Character charToAdd = buffer.charAt(j);
+      lastViewedChars.put(charToAdd, lastViewedChars.getOrDefault(charToAdd, 0) + 1);
+      if (lastViewedChars.size() == NEEDED_CONSECUTIVE_CHARACTERS)
+        return j + 1;
+    }
+
+    return -1;
   }
 
   @Override
