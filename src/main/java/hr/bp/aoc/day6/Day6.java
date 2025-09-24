a2be965468f69d81f06bbc0968d3eaf0dd586a2d
Karlo Cehulic <karlocehlic@gmail.com>
2025-09-24 20:40:36 +0200

    Add class for Day6

---
+++ src/main/java/hr/bp/aoc/day6/Day6.java
@@ -0,0 +1,27 @@
+package hr.bp.aoc.day6;
+
+import java.io.IOException;
+
+import hr.bp.aoc.DaySolution;
+
+public class Day6 extends DaySolution<Integer, Integer> {
+
+  public Day6() throws IOException {
+    super();
+  }
+
+  @Override
+  public String getInputFileName() {
+    return "subroutine.txt";
+  }
+
+  @Override
+  public Integer getPart1Solution() {
+    return 0;
+  }
+
+  @Override
+  public Integer getPart2Solution() {
+    return 0;
+  }
+}
