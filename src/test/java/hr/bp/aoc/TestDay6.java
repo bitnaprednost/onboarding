1263775fc46d739b4f6734c89354cf9db380d241
Karlo Cehulic <karlocehlic@gmail.com>
2025-09-24 21:30:30 +0200

    test: Add day 6 part 1 test

---
+++ src/test/java/hr/bp/aoc/TestDay6.java
@@ -0,0 +1,19 @@
+package hr.bp.aoc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import hr.bp.aoc.day6.Day6;
+
+public class TestDay6 {
+
+  @Test
+  public void testPart6() throws IOException {
+    Day6 day6 = new Day6();
+
+    assertEquals(1794, day6.getPart1Solution());
+  }
+}
