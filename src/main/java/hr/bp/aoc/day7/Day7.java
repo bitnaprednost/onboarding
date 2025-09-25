27f434e761b20f7c028b0c57a398632b1e00eccc
Karlo Cehulic <karlocehlic@gmail.com>
2025-09-25 22:26:00 +0200

    refactor: Extract Dir into separate class

--- src/main/java/hr/bp/aoc/day7/Day7.java
+++ src/main/java/hr/bp/aoc/day7/Day7.java
@@ -1,64 +1,12 @@
 package hr.bp.aoc.day7;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Stack;
 
 import hr.bp.aoc.DaySolution;
 
 public class Day7 extends DaySolution<Integer, Integer> {
-  public class Dir {
-    private Dir parent;
-    private int fileSize = 0;
-    private int fullSize = -1;
-    private Map<String, Dir> children = new HashMap<>();
-
-    public Dir(Dir parent, String name) {
-      this.parent = parent;
-    }
-
-    public Dir getParent() {
-      return parent;
-    }
-
-    public Dir getChild(String name) {
-      return children.get(name);
-    }
-
-    public void parseChildren(List<String> ls) {
-
-      for (String line : ls) {
-        String prefix = line.split(" ")[0];
-        String name = line.split(" ")[1];
-
-        if ("dir".equals(prefix)) {
-          children.put(name, new Dir(this, name));
-        } else {
-          fileSize += Integer.parseInt(prefix);
-        }
-      }
-    }
-
-    public int getFullSize() {
-      if (fullSize != -1)
-        return fullSize;
-
-      int dirSize = 0;
-      for (Dir child : children.values()) {
-        dirSize += child.getFullSize();
-      }
-
-      return dirSize + fileSize;
-    }
-
-    public Collection<Dir> getChildren() {
-      return children.values();
-    }
-  }
-
   public Day7() throws IOException {
     super();
   }
