6770e9495b6dc2f0596583612445e6eadafaaafa
Karlo Cehulic <karlocehlic@gmail.com>
2025-09-25 18:11:38 +0200

    Add day 7 solution boilerplate code

---
+++ src/main/java/hr/bp/aoc/day7/Day7.java
@@ -0,0 +1,26 @@
+package hr.bp.aoc.day7;
+
+import java.io.IOException;
+
+import hr.bp.aoc.DaySolution;
+
+public class Day7 extends DaySolution<Integer, Integer> {
+  public Day7() throws IOException {
+    super();
+  }
+
+  @Override
+  public String getInputFileName() {
+    return "filesystem.txt";
+  }
+
+  @Override
+  public Integer getPart1Solution() {
+    return 0;
+  }
+
+  @Override
+  public Integer getPart2Solution() {
+    return 0;
+  }
+}
