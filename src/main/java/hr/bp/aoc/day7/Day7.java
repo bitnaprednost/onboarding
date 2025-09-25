bc692d494434d1ed7240fd71c129a800cac60b83
Karlo Cehulic <karlocehlic@gmail.com>
2025-09-25 22:38:10 +0200

    refactor: Create FileManager class

--- src/main/java/hr/bp/aoc/day7/Day7.java
+++ src/main/java/hr/bp/aoc/day7/Day7.java
@@ -1,8 +1,6 @@
 package hr.bp.aoc.day7;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Stack;
 
 import hr.bp.aoc.DaySolution;
 
@@ -18,56 +16,10 @@ public class Day7 extends DaySolution<Integer, Integer> {
 
   @Override
   public Integer getPart1Solution() {
-    Dir head = new Dir(null, "/");
-    Dir curr = head;
+    FileManager fileManager = new FileManager();
+    fileManager.createFilesystemFromTerminal(getInputLines());
 
-    List<String> lines = getInputLines();
-    int i = 1;
-    while (i < lines.size()) {
-      String[] splited = lines.get(i).split(" ");
-
-      boolean isCommand = "$".equals(splited[0]);
-
-      if (isCommand) {
-        boolean isCd = "cd".equals(splited[1]);
-        if (isCd) {
-          String destination = splited[2];
-          boolean isReturning = "..".equals(destination);
-          if (isReturning) {
-            curr = curr.getParent();
-          } else {
-            curr = curr.getChild(destination);
-          }
-        } else {
-          i++;
-          int startingDirIndex = i;
-          boolean isEndOfDirLs;
-          do {
-            i++;
-            isEndOfDirLs = i == lines.size() || "$".equals(lines.get(i).split(" ")[0]);
-          } while (!isEndOfDirLs);
-
-          curr.parseChildren(lines.subList(startingDirIndex, i));
-          i--;
-        }
-      }
-
-      i++;
-    }
-
-    int res = 0;
-    Stack<Dir> stack = new Stack<>();
-    stack.push(head);
-    while (!stack.empty()) {
-      Dir currCounting = stack.pop();
-      if (currCounting.getFullSize() <= 100000)
-        res += currCounting.getFullSize();
-      for (Dir child : currCounting.getChildren()) {
-        stack.push(child);
-      }
-    }
-
-    return res;
+    return fileManager.sumOfSizesSmallerThan100000();
   }
 
   @Override
---
+++ src/main/java/hr/bp/aoc/day7/FileManager.java
@@ -0,0 +1,65 @@
+package hr.bp.aoc.day7;
+
+import java.util.List;
+import java.util.Stack;
+
+public class FileManager {
+  private Dir head;
+
+  public FileManager() {
+    head = new Dir(null, "/");
+  }
+
+  public void createFilesystemFromTerminal(List<String> terminalLines) {
+    Dir curr = head;
+
+    int i = 1;
+    while (i < terminalLines.size()) {
+      String[] splited = terminalLines.get(i).split(" ");
+
+      boolean isCommand = "$".equals(splited[0]);
+
+      if (isCommand) {
+        boolean isCd = "cd".equals(splited[1]);
+        if (isCd) {
+          String destination = splited[2];
+          boolean isReturning = "..".equals(destination);
+          if (isReturning) {
+            curr = curr.getParent();
+          } else {
+            curr = curr.getChild(destination);
+          }
+        } else {
+          i++;
+          int startingDirIndex = i;
+          boolean isEndOfDirLs;
+          do {
+            i++;
+            isEndOfDirLs = i == terminalLines.size() || "$".equals(terminalLines.get(i).split(" ")[0]);
+          } while (!isEndOfDirLs);
+
+          curr.parseChildren(terminalLines.subList(startingDirIndex, i));
+          i--;
+        }
+      }
+
+      i++;
+    }
+  }
+
+  public int sumOfSizesSmallerThan100000() {
+    int sum = 0;
+    Stack<Dir> stack = new Stack<>();
+    stack.push(head);
+    while (!stack.empty()) {
+      Dir currCounting = stack.pop();
+      if (currCounting.getFullSize() <= 100000)
+        sum += currCounting.getFullSize();
+      for (Dir child : currCounting.getChildren()) {
+        stack.push(child);
+      }
+    }
+
+    return sum;
+  }
+}
