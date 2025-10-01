ccb4d5d0fde5a3cf719c798c991f6765dba35637
Karlo Cehulic <karlocehlic@gmail.com>
2025-10-01 08:09:02 +0200

    Add day 8 boilerplate

---
+++ src/main/java/hr/bp/aoc/day8/Day8.java
@@ -0,0 +1,26 @@
+package hr.bp.aoc.day8;
+
+import java.io.IOException;
+
+import hr.bp.aoc.DaySolution;
+
+public class Day8 extends DaySolution<Integer, Integer> {
+  public Day8() throws IOException {
+    super();
+  }
+
+  @Override
+  public String getInputFileName() {
+    return "treemap.txt";
+  }
+
+  @Override
+  public Integer getPart1Solution() {
+    return 0;
+  }
+
+  @Override
+  public Integer getPart2Solution() {
+    return 0;
+  }
+}
